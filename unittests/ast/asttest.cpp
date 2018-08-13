28ebb2fbddc6c51f63f727e4ac8e575b739e4395
Dwight Guth <dwight.guth@runtimeverification.com>
2018-08-13 13:57:44 -0500

    add test for instantiateSymbol

--- unittests/ast/asttest.cpp
+++ unittests/ast/asttest.cpp
@@ -20,4 +20,25 @@ BOOST_AUTO_TEST_CASE(substitute) {
   BOOST_CHECK_EQUAL(*poly->substitute(subst), *expected);
 }
 
+BOOST_AUTO_TEST_CASE(instantiate) {
+  auto decl = KOREObjectSymbolDeclaration::Create("sym");
+  auto var = KOREObjectSortVariable::Create("foo");
+  decl->addObjectSortVariable(var);
+  auto poly = KOREObjectCompositeSort::Create("baz");
+  auto composite = KOREObjectCompositeSort::Create("bar");
+  poly->addArgument(var);
+  decl->getSymbol()->addArgument(poly);
+  decl->getSymbol()->addArgument(composite);
+  decl->getSymbol()->addSort(poly);
+  auto sym = KOREObjectSymbol::Create("sym");
+  sym->addArgument(composite);
+  sym->instantiateSymbol(decl);
+  auto expected = KOREObjectCompositeSort::Create("baz");
+  expected->addArgument(composite);
+  BOOST_CHECK_EQUAL(*sym->getSort(), *expected);
+  BOOST_CHECK_EQUAL(sym->getArguments().size(), 2);
+  BOOST_CHECK_EQUAL(*sym->getArguments()[0], *expected);
+  BOOST_CHECK_EQUAL(*sym->getArguments()[1], *composite);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
