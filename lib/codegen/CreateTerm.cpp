8edc6141f9953f6848b765c398d8d57cf1c692e9
Dwight Guth <dwight.guth@runtimeverification.com>
2018-08-16 15:04:49 -0500

    add proper layout string

--- lib/codegen/CreateTerm.cpp
+++ lib/codegen/CreateTerm.cpp
@@ -13,6 +13,10 @@
 namespace kllvm {
 
 std::string LLVM_HEADER = R"LLVM(
+; Linux target
+target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-unknown-linux-gnu"
+
 ; K types in LLVM
 
 ; A K value in the LLVM backend can be one of the following values:
