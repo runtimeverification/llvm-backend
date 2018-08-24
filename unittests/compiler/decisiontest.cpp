6e34fd5e68548ac450bd27353a6ef542ae8990e6
Dwight Guth <dwight.guth@runtimeverification.com>
2018-08-24 14:47:41 -0500

    add new test to decisiontest.cpp

--- unittests/compiler/decisiontest.cpp
+++ unittests/compiler/decisiontest.cpp
@@ -11,69 +11,7 @@ using namespace kllvm;
 
 BOOST_AUTO_TEST_SUITE(DecisionTest)
 
-BOOST_AUTO_TEST_CASE(simple) {
-/* match subject with 
-   | Foo(l,r) -> match l with
-                 | Bar(b) -> match b with
-                             | true -> apply_rule_0()
-                             | false -> apply_rule_1()
-                 | Baz -> let b = eval_foo(r) in apply_rule_2(r, b)
-   | _ -> fail()
-*/
-  auto Literal = SwitchNode::Create("b");
-  auto dv = KOREObjectSymbol::Create("\\dv");
-
-  Literal->addCase({dv, llvm::APInt(1, 1), LeafNode::Create("apply_rule_0")});
-  Literal->addCase({dv, llvm::APInt(1, 0), LeafNode::Create("apply_rule_1")});
-
-  auto Inner = SwitchNode::Create("l");
-  auto Bar = KOREObjectSymbol::Create("Bar");
-  auto Baz = KOREObjectSymbol::Create("Baz");
-  auto InnerSort = KOREObjectCompositeSort::Create("Innor", SortCategory::Symbol);
-  auto Bool = KOREObjectCompositeSort::Create("Bool", SortCategory::Bool);
-
-  Bar->addSort(InnerSort);
-  Bar->addArgument(Bool);
-  Baz->addSort(InnerSort);
-  Bar->setTag(0);
-  Baz->setTag(1);
-
-  DecisionCase BarCase{Bar, {"b"}, Literal};
-  Inner->addCase(BarCase);
-  auto BazLeaf = LeafNode::Create("apply_rule_2");
-  BazLeaf->addBinding("r");
-  BazLeaf->addBinding("b");
-  auto Func = FunctionNode::Create("b", "eval_foo", BazLeaf, SortCategory::Bool);
-  Func->addBinding("r");
-  Inner->addCase({Baz, std::vector<std::string>{}, Func});
-
-  auto Outer = SwitchNode::Create("subject0");
-  auto Foo = KOREObjectSymbol::Create("Foo");
-  auto OuterSort = KOREObjectCompositeSort::Create("Outer", SortCategory::Symbol);
-  auto Int = KOREObjectCompositeSort::Create("Int", SortCategory::Int);
-
-  Foo->addSort(OuterSort);
-  Foo->addArgument(InnerSort);
-  Foo->addArgument(Int);
-  Foo->setTag(2);
-
-  DecisionCase InnerCase{Foo, {"l", "r"}, Inner};
-  Outer->addCase(InnerCase);
-  Outer->addCase(DecisionCase{nullptr, std::vector<std::string>{}, FailNode::get()});
-
-  llvm::LLVMContext Ctx;
-  auto mod = newModule("test_decision", Ctx);
-
-  auto F = KOREObjectSymbol::Create("func");
-  F->addSort(Int);
-  F->addArgument(OuterSort);
-
-  makeEvalFunction(F, nullptr, mod.get(), Outer);
-
-  std::string actual;
-  llvm::raw_string_ostream out(actual);
-  mod->print(out, nullptr);
-  BOOST_CHECK_EQUAL(actual, R"(target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
+static std::string EXPECTED = R"(target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
 target triple = "x86_64-unknown-linux-gnu"
 
 %block = type { %blockheader, [0 x i64*] }
@@ -88,17 +26,17 @@ entry:
   %1 = trunc i64 %0 to i1
   br i1 %1, label %constant, label %block
 
-stuck:                                            ; preds = %l_case_0, %getTag3, %getTag
+stuck:                                            ; preds = %_0_case_0, %getTag3, %getTag
   call void @abort()
   unreachable
 
 subject0_case_0:                                  ; preds = %getTag
   %2 = bitcast %block* %subject0 to { %blockheader, [0 x i64], %block*, %mpz* }*
   %3 = getelementptr inbounds { %blockheader, [0 x i64], %block*, %mpz* }, { %blockheader, [0 x i64], %block*, %mpz* }* %2, i64 0, i32 2
-  %l = load %block*, %block** %3
+  %_0 = load %block*, %block** %3
   %4 = getelementptr inbounds { %blockheader, [0 x i64], %block*, %mpz* }, { %blockheader, [0 x i64], %block*, %mpz* }* %2, i64 0, i32 3
-  %r = load %mpz*, %mpz** %4
-  %5 = ptrtoint %block* %l to i64
+  %_1 = load %mpz*, %mpz** %4
+  %5 = ptrtoint %block* %_0 to i64
   %6 = trunc i64 %5 to i1
   br i1 %6, label %constant1, label %block2
 
@@ -118,19 +56,19 @@ getTag:                                           ; preds = %block, %constant
     i32 2, label %subject0_case_0
   ]
 
-l_case_0:                                         ; preds = %getTag3
-  %11 = bitcast %block* %l to { %blockheader, [0 x i64], i1 }*
+_0_case_0:                                        ; preds = %getTag3
+  %11 = bitcast %block* %_0 to { %blockheader, [0 x i64], i1 }*
   %12 = getelementptr inbounds { %blockheader, [0 x i64], i1 }, { %blockheader, [0 x i64], i1 }* %11, i64 0, i32 2
-  %b = load i1, i1* %12
-  switch i1 %b, label %stuck [
-    i1 true, label %b_case_0
-    i1 false, label %b_case_1
+  %_2 = load i1, i1* %12
+  switch i1 %_2, label %stuck [
+    i1 true, label %_2_case_0
+    i1 false, label %_2_case_1
   ]
 
-l_case_1:                                         ; preds = %getTag3
-  %13 = bitcast %block* %l to { %blockheader, [0 x i64] }*
-  %b5 = call i1 @eval_foo(%mpz* %r)
-  %14 = call %mpz* @apply_rule_2(%mpz* %r, i1 %b5)
+_0_case_1:                                        ; preds = %getTag3
+  %13 = bitcast %block* %_0 to { %blockheader, [0 x i64] }*
+  %_3 = call i1 @eval_foo(%mpz* %_1)
+  %14 = call %mpz* @apply_rule_2(%mpz* %_1, i1 %_3)
   ret %mpz* %14
 
 constant1:                                        ; preds = %subject0_case_0
@@ -138,7 +76,7 @@ constant1:                                        ; preds = %subject0_case_0
   br label %getTag3
 
 block2:                                           ; preds = %subject0_case_0
-  %16 = getelementptr inbounds %block, %block* %l, i64 0, i32 0, i32 0
+  %16 = getelementptr inbounds %block, %block* %_0, i64 0, i32 0, i32 0
   %17 = load i64, i64* %16
   br label %getTag3
 
@@ -146,15 +84,15 @@ getTag3:                                          ; preds = %block2, %constant1
   %phi4 = phi i64 [ %17, %block2 ], [ %15, %constant1 ]
   %18 = trunc i64 %phi4 to i32
   switch i32 %18, label %stuck [
-    i32 0, label %l_case_0
-    i32 1, label %l_case_1
+    i32 0, label %_0_case_0
+    i32 1, label %_0_case_1
   ]
 
-b_case_0:                                         ; preds = %l_case_0
+_2_case_0:                                        ; preds = %_0_case_0
   %19 = call %mpz* @apply_rule_0()
   ret %mpz* %19
 
-b_case_1:                                         ; preds = %l_case_0
+_2_case_1:                                        ; preds = %_0_case_0
   %20 = call %mpz* @apply_rule_1()
   ret %mpz* %20
 }
