2b628bc8af3951b7728e3bdb0dc0d7b675b3314d
Radu Ometita <radu.ometita@gmail.com>
2018-08-27 18:48:56 +0200

    Get a list of rewrite patterns from the KORE file.

--- matching/src/Pattern/Parser.hs
+++ matching/src/Pattern/Parser.hs
@@ -1,19 +1,34 @@
+{-# LANGUAGE GADTs      #-}
 {-# LANGUAGE LambdaCase #-}
 module Pattern.Parser where
 
-import           Data.Map              (Map, findWithDefault, insert)
-import qualified Data.Map              as Map (empty)
-import           Data.Maybe            (catMaybes)
+import           Data.Functor.Foldable      (Fix (..), para)
+import           Data.Map                   (Map, findWithDefault, insert)
+import qualified Data.Map                   as Map (empty)
+import           Data.Maybe                 (catMaybes)
 
-import           Kore.AST.Common       (Symbol (..), Variable)
-import           Kore.AST.Kore         (UnifiedPattern, UnifiedSort,
-                                        UnifiedSortVariable)
-import           Kore.AST.MetaOrObject (Unified (..), asUnified)
-import           Kore.AST.Sentence     (Definition (..), KoreDefinition,
-                                        Module (..), Sentence (..),
-                                        SentenceSymbol (..), UnifiedSentence,
-                                        applyUnifiedSentence)
-import           Kore.Parser.Parser    (fromKore)
+import           Data.Functor.Impredicative (Rotate31 (..))
+import           Kore.AST.Common            (And (..), Application (..),
+                                             Ceil (..), Equals (..),
+                                             Exists (..), Floor (..),
+                                             Forall (..), Iff (..),
+                                             Implies (..), In (..), Next (..),
+                                             Not (..), Or (..), Pattern (..),
+                                             Rewrites (..), Symbol (..),
+                                             Variable)
+import           Kore.AST.Kore              (CommonKorePattern,
+                                             UnifiedPattern (..), UnifiedSort,
+                                             UnifiedSortVariable)
+import           Kore.AST.MetaOrObject      (Meta (..), Object (..),
+                                             Unified (..),
+                                             asUnified)
+import           Kore.AST.Sentence          (Definition (..), KoreDefinition,
+                                             Module (..), Sentence (..),
+                                             SentenceAxiom (..),
+                                             SentenceSymbol (..),
+                                             UnifiedSentence,
+                                             applyUnifiedSentence)
+import           Kore.Parser.Parser         (fromKore)
 
 --[ Metadata ]--
 
@@ -54,6 +69,57 @@ parseSymbols koreDefinition =
       sentences = mconcat (moduleSentences <$> modules)
       symbols   = catMaybes (parseSymbolSentence <$> sentences)
   in mkSymLib symbols
+
+--[ Patterns ]--
+
+parseAxiomSentence :: UnifiedSentence UnifiedSortVariable UnifiedPattern Variable
+                   -> [Rewrites Object (Fix (UnifiedPattern Variable))]
+parseAxiomSentence = applyUnifiedSentence metaT (const [])
+  where
+    metaT :: Sentence Meta UnifiedSortVariable UnifiedPattern Variable
+          -> [Rewrites Object (Fix (UnifiedPattern Variable))]
+    metaT = \case
+      SentenceAxiomSentence s ->
+        findRewrites (sentenceAxiomPattern s)
+      _ -> []
+
+unifiedPatternRAlgebra :: (Pattern Meta variable (CommonKorePattern, b) -> b)
+                       -> (Pattern Object variable (CommonKorePattern, b) -> b)
+                       -> (UnifiedPattern variable (CommonKorePattern, b) -> b)
+unifiedPatternRAlgebra metaT _ (UnifiedPattern (UnifiedMeta meta)) =
+  metaT (unRotate31 meta)
+unifiedPatternRAlgebra _ objectT (UnifiedPattern (UnifiedObject object)) =
+  objectT (unRotate31 object)
+
+findRewrites :: CommonKorePattern
+             -> [Rewrites Object CommonKorePattern]
+findRewrites = para (unifiedPatternRAlgebra rAlgebra rAlgebra)
+  where
+    rAlgebra :: Pattern lvl Variable (CommonKorePattern,
+                                      [Rewrites Object CommonKorePattern])
+             -> [Rewrites Object CommonKorePattern]
+    rAlgebra (RewritesPattern (Rewrites sort (first, _) (second, _))) =
+      [Rewrites sort first second]
+    rAlgebra (AndPattern (And _ (_, p₀) (_, p₁)))         = p₀ ++ p₁
+    rAlgebra (ApplicationPattern (Application _ ps))      = mconcat $ map snd ps
+    rAlgebra (CeilPattern (Ceil _ _ (_, p)))              = p
+    rAlgebra (EqualsPattern (Equals _ _ (_, p₀) (_, p₁))) = p₀ ++ p₁
+    rAlgebra (ExistsPattern (Exists _ _ (_, p)))          = p
+    rAlgebra (FloorPattern (Floor _ _ (_, p)))            = p
+    rAlgebra (ForallPattern (Forall _ _ (_, p)))          = p
+    rAlgebra (IffPattern (Iff _ (_, p₀) (_, p₁)))         = p₀ ++ p₁
+    rAlgebra (ImpliesPattern (Implies _ (_, p₀) (_, p₁))) = p₀ ++ p₁
+    rAlgebra (InPattern (In _ _ (_, p₀) (_, p₁)))         = p₀ ++ p₁
+    rAlgebra (NextPattern (Next _ (_, p)))                = p
+    rAlgebra (NotPattern (Not _ (_, p)))                  = p
+    rAlgebra (OrPattern (Or _ (_, p₀) (_, p₁)))           = p₀ ++ p₁
+    rAlgebra _                                            = []
+
+parseRewrites :: KoreDefinition -> [Rewrites Object CommonKorePattern]
+parseRewrites koreDefinition =
+  let modules   = definitionModules koreDefinition
+      sentences = mconcat (moduleSentences <$> modules)
+  in mconcat (parseAxiomSentence <$> sentences)
 
 parseDefinition :: FilePath -> IO KoreDefinition
 parseDefinition fileName = do
