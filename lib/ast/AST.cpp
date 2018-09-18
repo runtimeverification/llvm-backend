2336aa60d834a9199ab2384e759ff2ae2e94d763
Dwight Guth <dwight.guth@runtimeverification.com>
2018-09-18 12:10:24 -0500

    update for traian's change to k

--- lib/ast/AST.cpp
+++ lib/ast/AST.cpp
@@ -265,10 +265,11 @@ static const std::string COMM = "comm";
 static const std::string IDEM = "idem";
 static const std::string UNIT = "unit";
 static const std::string FUNCTIONAL = "functional";
+static const std::string SUBSORT = "subsort";
 static const std::string CONSTRUCTOR = "constructor";
 
 bool KOREAxiomDeclaration::isRequired() {
-  return !attributes.count(ASSOC) && !attributes.count(COMM) && !attributes.count(IDEM) && !attributes.count(UNIT) && !attributes.count(FUNCTIONAL) && !attributes.count(CONSTRUCTOR);
+  return !attributes.count(ASSOC) && !attributes.count(COMM) && !attributes.count(IDEM) && !attributes.count(UNIT) && !attributes.count(FUNCTIONAL) && !attributes.count(CONSTRUCTOR) && !attributes.count(SUBSORT);
 }
 
 KOREPattern *KOREAxiomDeclaration::getRightHandSide() const {
