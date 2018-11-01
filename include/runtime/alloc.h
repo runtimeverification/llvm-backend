6f19996cfcb8a0464c748b4880d2871de29ca278
Dwight Guth <dwight.guth@runtimeverification.com>
2018-11-01 12:45:39 -0500

    add comments to alloc.h

--- include/runtime/alloc.h
+++ include/runtime/alloc.h
@@ -8,11 +8,19 @@ extern "C" {
 #define mem_block_header(ptr) ((memory_block_header *)(((uintptr_t)(ptr)) & ~(BLOCK_SIZE-1)))
 
 extern const size_t BLOCK_SIZE;
+// allocates exactly requested bytes into the young generation
 void* koreAlloc(size_t requested);
+// allocates enough space for a string token whose raw size is requested into the young generation.
+// rounds up to the nearest 8 bytes and always allocates at least 16 bytes
 void* koreAllocToken(size_t requested);
+// allocates exactly requested bytes into the old generation
 void* koreAllocOld(size_t requested);
+// allocates enough space for a string token whose raw size is requested into the old generation.
+// rounds up to the nearest 8 bytes and always allocates at least 16 bytes
 void* koreAllocTokenOld(size_t requested);
+// swaps the two semispace of the young generation as part of garbage collection
 void koreAllocSwap(void);
+// resizes the last allocation into the young generation
 void* koreResizeLastAlloc(void* oldptr, size_t newrequest, size_t oldrequest);
 
 typedef struct {
