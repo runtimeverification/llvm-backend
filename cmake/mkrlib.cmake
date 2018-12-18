e683831929cf5be6abf3c7fe629a6f107f408760
Dwight Guth <dwight.guth@runtimeverification.com>
2018-12-18 13:22:22 -0600

    fix relative paths in cmake to use PROJECT_SOURCE_DIR

--- cmake/mkrlib.cmake
+++ cmake/mkrlib.cmake
@@ -1,5 +1,5 @@
 file(GLOB files ${CMAKE_INSTALL_PREFIX}/lib/kllvm/rust/deps/*.rlib)
 foreach(file ${files})
-  execute_process(COMMAND ../bin/llvm-kompile-rust-lto ${file})
+  execute_process(COMMAND ${CMAKE_INSTALL_PREFIX}/bin/llvm-kompile-rust-lto ${file})
 endforeach()
-execute_process(COMMAND ../bin/llvm-kompile-rust-lto ${CMAKE_INSTALL_PREFIX}/lib/kllvm/rust/libdatastructures.rlib)
+execute_process(COMMAND ${CMAKE_INSTALL_PREFIX}/bin/llvm-kompile-rust-lto ${CMAKE_INSTALL_PREFIX}/lib/kllvm/rust/libdatastructures.rlib)
