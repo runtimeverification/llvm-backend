d1bc82361df878bc60e9227ee4490bb1ff1f5c71
Dwight Guth <dwight.guth@runtimeverification.com>
2019-01-11 13:42:17 -0600

    printing float constants

--- runtime/configurationparser/ConfigurationPrinter.cpp
+++ runtime/configurationparser/ConfigurationPrinter.cpp
@@ -1,16 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cinttypes>
 
 #include "runtime/header.h"
+#include "runtime/alloc.h"
 
 void printInt(FILE *file, mpz_t i, const char *sort) {
   char *str = mpz_get_str(NULL, 10, i);
   fprintf(file, "\\dv{%s}(\"%s\")", sort, str);
 }
 
-void printFloat(FILE *file, mpfr_t f, const char *sort) {
-  //TODO: print float
-  abort();
+void printFloat(FILE *file, floating *f, const char *sort) {
+  uint64_t prec = mpfr_get_prec(f->f);
+  uint64_t exp = f->exp;
+  char suffix[41]; // 19 chars per long + p and x and null byte
+  if (prec == 53 && exp == 11) {
+    suffix[0] = 0;
+  } else if (prec == 24 && exp == 8) {
+    suffix[0] = 'f';
+    suffix[1] = 0;
+  } else {
+    sprintf(suffix, "p%" PRIu64 "x%" PRIu64, prec, exp);
+  }
+  if (mpfr_nan_p(f->f)) {
+    fprintf(file, "\\dv{%s}(\"NaN%s\")", sort, suffix);
+  } else if (mpfr_inf_p(f->f)) {
+    if (mpfr_signbit(f->f)) {
+      fprintf(file, "\\dv{%s}(\"-Infinity%s\")", sort, suffix);
+    } else {
+      fprintf(file, "\\dv{%s}(\"Infinity%s\")", sort, suffix);
+    }
+  } else {
+    mpfr_exp_t printed_exp;
+    char *str = mpfr_get_str(NULL, &printed_exp, 10, 0, f->f, MPFR_RNDN);
+    size_t len = strlen(str);
+    char *newstr = (char *)koreAllocOld(len+2);
+    size_t idx = 0;
+    if (str[0] == '-') {
+      newstr[0] = '-';
+      idx = 1;
+    }
+    newstr[idx] = '0';
+    newstr[idx+1] = '.';
+    strcpy(newstr+idx+2,str+idx);
+    fprintf(file, "\\dv{%s}(\"%se%ld%s\")", sort, newstr, printed_exp, suffix);
+  }
 }
 
 void printBool(FILE *file, bool b, const char *sort) {
