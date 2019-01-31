4b8a403be85332c73ce511552fd4480308630b7d
Dwight Guth <dwight.guth@runtimeverification.com>
2019-01-31 20:48:17 +0000

    exclude another category of axiom

--- lib/ast/AST.cpp
+++ lib/ast/AST.cpp
@@ -276,9 +276,10 @@ static const std::string UNIT = "unit";
 static const std::string FUNCTIONAL = "functional";
 static const std::string SUBSORT = "subsort";
 static const std::string CONSTRUCTOR = "constructor";
+static const std::string CEIL = "ceil";
 
 bool KOREAxiomDeclaration::isRequired() {
-  return !attributes.count(ASSOC) && !attributes.count(COMM) && !attributes.count(IDEM) && !attributes.count(UNIT) && !attributes.count(FUNCTIONAL) && !attributes.count(CONSTRUCTOR) && !attributes.count(SUBSORT);
+  return !attributes.count(ASSOC) && !attributes.count(COMM) && !attributes.count(IDEM) && !attributes.count(UNIT) && !attributes.count(FUNCTIONAL) && !attributes.count(CONSTRUCTOR) && !attributes.count(SUBSORT) && !attributes.count(CEIL);
 }
 
 KOREPattern *KOREAxiomDeclaration::getRightHandSide() const {
