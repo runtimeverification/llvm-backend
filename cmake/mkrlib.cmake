6ec0694fe63f5c552ea3b64022ff8ba197d1252d
dwightguth <dwight.guth@runtimeverification.com>
2019-04-10 14:13:26 -0500

    Lto rust standard library (#113)
    * use lld for linker
    
    * explicitly list rust libraries
    
    * fix bug in llvm-kompile-rust-lto which incorrectly processes
    libbacktrace_sys
    
    * rust lto rust standard library
    
    * add missing rust library

--- cmake/mkrlib.cmake
+++ cmake/mkrlib.cmake
@@ -1,4 +1,4 @@
-file(GLOB files ${CMAKE_INSTALL_PREFIX}/lib/kllvm/rust/deps/*.rlib)
+file(GLOB files "${CMAKE_INSTALL_PREFIX}/lib/kllvm/rust/deps/*.rlib" "${CMAKE_INSTALL_PREFIX}/lib/kllvm/rust/deps/stdlib/*.rlib")
 foreach(file ${files})
   execute_process(COMMAND ${CMAKE_INSTALL_PREFIX}/bin/llvm-kompile-rust-lto ${file})
 endforeach()
