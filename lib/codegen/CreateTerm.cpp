85b9d9254b981efc3cbabfe6527533bb2da8d4be
Theodoros Kasampalis <kasampalis.theodoros@gmail.com>
2019-06-07 13:42:26 -0500

    fix indentation issue

--- lib/codegen/CreateTerm.cpp
+++ lib/codegen/CreateTerm.cpp
@@ -441,9 +441,9 @@ llvm::Value *CreateTerm::createFunctionCall(std::string name, KOREObjectComposit
     case SortCategory::List:
     case SortCategory::Set: {
       if (!arg->getType()->isPointerTy()) {
-          llvm::AllocaInst *AllocCollection = new llvm::AllocaInst(arg->getType(), 0, "", CurrentBlock);
-          new llvm::StoreInst(arg, AllocCollection, CurrentBlock);
-          args.push_back(AllocCollection);
+        llvm::AllocaInst *AllocCollection = new llvm::AllocaInst(arg->getType(), 0, "", CurrentBlock);
+        new llvm::StoreInst(arg, AllocCollection, CurrentBlock);
+        args.push_back(AllocCollection);
       } else {
         args.push_back(arg);
       }
@@ -470,7 +470,7 @@ llvm::Value *CreateTerm::createFunctionCall(std::string name, ValueType returnCa
     break;
   }
   llvm::Value *AllocSret;
- for (int i = 0; i < args.size(); i++) {
+  for (int i = 0; i < args.size(); i++) {
     llvm::Value *arg = args[i];
     types.push_back(arg->getType());
   }
