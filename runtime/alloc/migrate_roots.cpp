8e161787f0de3bc83912ecb30d396aad2819c5a7
Theodoros Kasampalis <kasampalis.theodoros@gmail.com>
2019-06-12 17:06:30 -0500

    Added support for multiple gc roots

---
+++ runtime/alloc/migrate_roots.cpp
@@ -0,0 +1,28 @@
+#include "runtime/header.h"
+#include "runtime/collect.h"
+
+static BlockEnumerator blockEnumerator = nullptr;
+
+void registerGCRootsEnumerator(BlockEnumerator f) {
+  blockEnumerator = f;
+}
+
+extern "C" {
+
+  void migrate(block** blockPtr);
+
+  void migrateRoots() {
+    if (!blockEnumerator) {
+      return;
+    }
+
+    auto BlockIteratorPair = (*blockEnumerator)();
+    block_iterator BlockStartIt = BlockIteratorPair.first;
+    block_iterator BlockEndIt = BlockIteratorPair.second;
+    
+    for (block_iterator it =  BlockStartIt; it != BlockEndIt; ++it) {
+      migrate(&*it);
+    }
+  }
+}
+
