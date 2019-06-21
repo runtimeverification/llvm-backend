55f4a0aa2b825ca6747a33c91b504a595ea6ce77
Micky Abir <micky.abir@gmail.com>
2019-06-21 16:38:03 -0500

    undo cherry-pick

--- matching/tree_stats.py
+++ matching/tree_stats.py
@@ -5,11 +5,9 @@ import sys
 
 node_count = 0
 node_depth = 0
-num_actions = 9
-sum_depth = 0
 
 def count_nodes(data, depth):
-  global node_count, node_depth, sum_depth, num_actions
+  global node_count, node_depth
   node_count += 1
   if node_depth < depth:
     node_depth = depth
@@ -20,9 +18,7 @@ def count_nodes(data, depth):
       for case in data["specializations"]:
         count_nodes(case[1], depth+1)
       count_nodes(data["default"], depth+1)
-    elif "action" in data:
-      num_actions += 1
-      sum_depth += depth
+    elif "action" in data: pass
   elif data == "fail" or data == None: pass
   else:
     print(type(data))
@@ -34,7 +30,6 @@ with open(sys.argv[1], 'r') as stream:
     doc = yaml.safe_load(stream)
     count_nodes(doc, 1)
     print("Size: " + str(node_count))
-    print("Max path length: " + str(node_depth))
-    print("Average path length: " + str(sum_depth / num_actions))
+    print("Path length: " + str(node_depth))
   except yaml.YAMLError as exc:
     print(exc)
