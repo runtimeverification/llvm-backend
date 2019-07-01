7a478363add5e619f17b83b5921902c03d94b237
Dwight Guth <dwight.guth@runtimeverification.com>
2019-07-01 18:06:13 +0000

    iterated guarded by flag

--- matching/src/main/scala/org/kframework/backend/llvm/matching/Matching.scala
+++ matching/src/main/scala/org/kframework/backend/llvm/matching/Matching.scala
@@ -8,7 +8,7 @@ import java.io.File
 import java.io.FileWriter
 
 object Matching {
-  def writeDecisionTreeToFile(filename: File, heuristic: String, outputFolder: File) {
+  def writeDecisionTreeToFile(filename: File, heuristic: String, outputFolder: File, iterated: Boolean) {
     val defn = new TextToKore().parse(filename)
     outputFolder.mkdirs()
     val allAxioms = Parser.getAxioms(defn).zipWithIndex
@@ -19,12 +19,6 @@ object Matching {
     } else {
       Generator.mkDecisionTree(symlib, defn, axioms, Seq(axioms.head.rewrite.sort))
     }
-    val specialDts = axioms.map(a => {
-      if (logging) {
-        System.out.println("Compiling decision tree for axiom " + a.ordinal)
-      }
-      Generator.mkSpecialDecisionTree(symlib, defn, axioms, Seq(axioms.head.rewrite.sort), a)
-    })
     val funcAxioms = Parser.parseFunctionAxioms(allAxioms)
     val sortedFuncAxioms = symlib.functions.map(funcAxioms.getOrElse(_, IndexedSeq()))
     val sorts = symlib.functions.map(symlib.signatures(_)._1)
@@ -36,12 +30,20 @@ object Matching {
     })
     val path = new File(outputFolder, "dt.yaml")
     dt.serializeToYaml(path)
-    val specialFiles = (axioms, specialDts).zipped.toIterable
-    for (pair <- specialFiles) {
-      val ordinal = pair._1.ordinal
-      val filename = "dt_" + ordinal + ".yaml"
-      if (pair._2.isDefined) {
-        pair._2.get._1.serializeToYaml(new File(outputFolder, filename), pair._2.get._2)
+    if (iterated) {
+      val specialDts = axioms.map(a => {
+        if (logging) {
+          System.out.println("Compiling decision tree for axiom " + a.ordinal)
+        }
+        Generator.mkSpecialDecisionTree(symlib, defn, axioms, Seq(axioms.head.rewrite.sort), a)
+      })
+      val specialFiles = (axioms, specialDts).zipped.toIterable
+      for (pair <- specialFiles) {
+        val ordinal = pair._1.ordinal
+        val filename = "dt_" + ordinal + ".yaml"
+        if (pair._2.isDefined) {
+          pair._2.get._1.serializeToYaml(new File(outputFolder, filename), pair._2.get._2)
+        }
       }
     }
     val files = (symlib.functions, dts).zipped.toIterable
@@ -61,7 +63,7 @@ object Matching {
   def main(args: Array[String]): Unit = {
     val file = new File(args(0))
     val outputFolder = new File(args(2))
-    logging = args.size > 3
-    writeDecisionTreeToFile(file, args(1), outputFolder)
+    logging = args.size > 4
+    writeDecisionTreeToFile(file, args(1), outputFolder, args(3) == "on")
   }
 }
