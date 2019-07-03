7ab499866dc95c5d78a457fc0adb32559ef325eb
Dwight Guth <dwight.guth@runtimeverification.com>
2019-07-03 01:08:14 +0000

    tree stats

--- matching/tree_stats.py
+++ matching/tree_stats.py
@@ -3,15 +3,11 @@
 import yaml
 import sys
 from yaml import CLoader as Loader
+from decimal import Decimal
 
-node_count = 0
-node_depth = 0
-num_actions = 0
-sum_depth = 0
-
-blank_result  = {"count": 0, "shared_count": 0, "max_depth": 0, "num_actions": 0, "sum_depth": 0}
-leaf_result   = {"count": 0, "shared_count": 0, "max_depth": 1, "num_actions": 0, "sum_depth": 0}
-action_result = {"count": 1, "shared_count": 1, "max_depth": 1, "num_actions": 1, "sum_depth": 1}
+blank_result  = {"count": 0, "shared_count": 0, "max_depth": 0, "max_choices": 0, "num_actions": 0, "sum_depth": 0, "sum_choices": 0}
+leaf_result   = {"count": 0, "shared_count": 0, "max_depth": 1, "max_choices": 0, "num_actions": 0, "sum_depth": 0, "sum_choices": 0}
+action_result = {"count": 1, "shared_count": 1, "max_depth": 1, "max_choices": 0, "num_actions": 1, "sum_depth": 1, "sum_choices": 0}
 
 def count_nodes_shared(data):
     if isinstance(data, dict):
@@ -25,6 +21,9 @@ def count_nodes_shared(data):
         result["shared_count"] += 1
         result["max_depth"] += 1
         result["sum_depth"] += result["num_actions"]
+        if "collection" in data:
+          result["sum_choices"] += result["num_actions"]
+          result["max_choices"] += 1
         data["result"] = result
         return result
       elif "specializations" in data:
@@ -34,14 +33,18 @@ def count_nodes_shared(data):
           result["count"] += case_result["count"]
           result["shared_count"] += case_result["shared_count"]
           result["max_depth"] = max(case_result["max_depth"] + 1, result["max_depth"])
+          result["max_choices"] = max(case_result["max_choices"], result["max_choices"])
           result["num_actions"] += case_result["num_actions"]
           result["sum_depth"] += case_result["sum_depth"] + case_result["num_actions"]
+          result["sum_choices"] += case_result["sum_choices"]
         case_result = count_nodes_shared(data["default"])
         result["count"] += case_result["count"] + 1
         result["shared_count"] += case_result["shared_count"] + 1
         result["max_depth"] = max(case_result["max_depth"] + 1, result["max_depth"])
+        result["max_choices"] = max(case_result["max_choices"], result["max_choices"])
         result["num_actions"] += case_result["num_actions"]
         result["sum_depth"] += case_result["sum_depth"] + case_result["num_actions"]
+        result["sum_choices"] += case_result["sum_choices"]
         data["result"] = result
         return result
       elif "action" in data:
@@ -62,6 +65,8 @@ with open(sys.argv[1], 'r') as stream:
     print("Size: " + "{:,}".format(result["count"]))
     print("Shared size: " + "{:,}".format(result["shared_count"]))
     print("Max path length: " + str(result["max_depth"]))
-    print("Average path length: " + str(result["sum_depth"] / result["num_actions"]))
+    print("Average path length: " + str(Decimal(result["sum_depth"]) / Decimal(result["num_actions"])))
+    print("Average number of choices: " + str(Decimal(result["sum_choices"]) / Decimal(result["num_actions"])))
+    print("Max number of choices: " + str(result["max_choices"]))
   except yaml.YAMLError as exc:
     print(exc)
