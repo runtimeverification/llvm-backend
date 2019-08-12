1673acb956c5ba9abc77489cbc0230e995d52117
Micky Abir <micky.abir@gmail.com>
2019-08-12 16:29:26 +0000

    started list hooks

---
+++ runtime/collections/lists.cpp
@@ -0,0 +1,54 @@
+#include "runtime/header.h"
+
+#include "immer/vector.hpp"
+#include "immer/vector_transient.hpp"
+
+using List = immer::vector<block *>;
+
+List hook_LIST_unit() {
+  return List();
+}
+
+List hook_LIST_element(block * value) {
+  return List{value};
+}
+
+List hok_LIST_concat(List l1, List l2) {
+  auto tmp = l1.transient();
+  for (auto iter = l2.begin(); iter != l2.end(); iter++) {
+    tmp.push_back(*iter);
+  }
+  return tmp.persistent();
+}
+
+bool hook_LIST_in(block * value, List list) {
+  for (auto iter = list.begin(); iter != list.end(); iter++) {
+    if ((*iter) == value) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool hook_LIST_in_keys(mpz_t index, List list) {
+  if (!mpz_fits_ulong_p(index)) {
+    throw std::invalid_argument("Index is too large");
+  }
+  size_t idx = mpz_get_ui(index);
+  return idx < list.size();
+}
+
+block * hook_LIST_get_long(List list, mpz_t index) {
+  if (!mpz_fits_slong_p(index)) {
+    throw std::invalid_argument("Index is too large");
+  }
+  size_t size = list.size();
+  size_t idx = mpz_get_ui(index);
+  size_t abs_idx = idx < 0 ? size + idx : idx;
+
+  return list.at(abs_idx);
+}
+
+block * hook_LIST_get(List list, mpz_t index) {
+  return hook_LIST_get_long(list, index);
+}
