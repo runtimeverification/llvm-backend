14ab28d2e7003df65b1ea5206ccdda999a044f9a
Dwight Guth <dwight.guth@runtimeverification.com>
2019-09-06 16:09:16 +0000

    refactor includes in former c file

--- runtime/collect/collect.cpp
+++ runtime/collect/collect.cpp
@@ -1,9 +1,9 @@
-#include<stdbool.h>
-#include<stdint.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<assert.h>
+#include<cstdbool>
+#include<cstdint>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cassert>
 #include "runtime/alloc.h"
 #include "runtime/header.h"
 #include "runtime/arena.h"
