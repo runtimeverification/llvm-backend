38ad71688f917e7f6a0b4d0af5c4a14e34c037a0
Dwight Guth <dwight.guth@runtimeverification.com>
2019-09-11 21:33:07 +0000

    link depends

--- cmake/LLVMKompile.cmake
+++ cmake/LLVMKompile.cmake
@@ -9,19 +9,11 @@ set(CMAKE_RANLIB "${CMAKE_C_COMPILER_RANLIB}")
 
 find_program(LLVM_KOMPILE llvm-kompile)
 
-set(LLVM_KOMPILE_FLAGS "${CMAKE_CURRENT_BINARY_DIR}/definition.kore ${KOMPILED_DIR}/dt ${KOMPILE_USE_MAIN}")
+set(LLVM_KOMPILE_FLAGS "${KOMPILED_DIR}/definition.kore ${KOMPILED_DIR}/dt ${KOMPILE_USE_MAIN}")
 set(CMAKE_CXX_LINK_EXECUTABLE "${LLVM_KOMPILE} ${LLVM_KOMPILE_FLAGS} <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
 
 get_filename_component(K_BIN_DIR ${LLVM_KOMPILE} DIRECTORY)
 include_directories(${K_BIN_DIR}/../include)
 
-add_custom_command(
-	OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/definition.kore"
-	COMMAND "${CMAKE_COMMAND}" -E copy "${KOMPILED_DIR}/definition.kore" "${CMAKE_CURRENT_BINARY_DIR}"
-	DEPENDS "${KOMPILED_DIR}/definition.kore")
-add_custom_target(definition
-	DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/definition.kore")
-
 target_compile_options(${TARGET_NAME} -Wno-return-type-c-linkage)
-add_dependencies(${TARGET_NAME} definition)
-set_target_properties(${TARGET_NAME} PROPERTIES LINK_DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/definition.kore")
+set_target_properties(${TARGET_NAME} PROPERTIES LINK_DEPENDS "${KOMPILED_DIR}/definition.kore")
