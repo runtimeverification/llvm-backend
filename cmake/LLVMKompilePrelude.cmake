8b7f440c417998234b8ec03430c86560bbcbea74
dwightguth <dwight.guth@runtimeverification.com>
2019-09-12 11:41:43 -0500

    Add cmake script for building k definitions (#237)
    * add cmake script for k definitions
    
    * don't wall by default
    
    * custom target for definition.kore
    
    * also true
    
    * whole copy thing
    
    * link depends
    
    * link depends
    
    * fix
    
    * install

---
+++ cmake/LLVMKompilePrelude.cmake
@@ -0,0 +1,13 @@
+find_program(CMAKE_C_COMPILER clang-8)
+find_program(CMAKE_C_COMPILER clang-7)
+find_program(CMAKE_C_COMPILER clang)
+find_program(CMAKE_CXX_COMPILER clang++-8)
+find_program(CMAKE_CXX_COMPILER clang++-7)
+find_program(CMAKE_CXX_COMPILER clang++)
+find_program(CMAKE_C_COMPILER_AR llvm-ar-8)
+find_program(CMAKE_C_COMPILER_AR llvm-ar-7)
+find_program(CMAKE_C_COMPILER_AR llvm-ar)
+find_program(CMAKE_C_COMPILER_RANLIB llvm-ranlib-8)
+find_program(CMAKE_C_COMPILER_RANLIB llvm-ranlib-7)
+find_program(CMAKE_C_COMPILER_RANLIB llvm-ranlib)
+
