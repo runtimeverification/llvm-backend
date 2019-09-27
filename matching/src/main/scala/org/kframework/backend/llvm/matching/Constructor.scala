99643b1beaa6e54a84e5f5bf877c114364af2b39
Dwight Guth <dwight.guth@runtimeverification.com>
2019-09-27 19:16:58 +0000

    Constructor.contract: inverse of expand
    This is used by the exhaustiveness check to produce a counterexample
    from a specialization, a counterexample of the specialization, and a constructor

--- matching/src/main/scala/org/kframework/backend/llvm/matching/Constructor.scala
+++ matching/src/main/scala/org/kframework/backend/llvm/matching/Constructor.scala
@@ -1,18 +1,26 @@
 package org.kframework.backend.llvm.matching
 
-import org.kframework.backend.llvm.matching.pattern.Pattern
+import org.kframework.backend.llvm.matching.pattern._
 import org.kframework.parser.kore.SymbolOrAlias
 
 sealed trait Constructor {
   def name: String
   def isBest(pat: Pattern[Option[Occurrence]]): Boolean
   def expand(f: Fringe): Option[Seq[Fringe]]
+  def contract(f: Fringe, children: Seq[Pattern[String]]): Pattern[String]
 }
 
 case class Empty() extends Constructor {
   def name = "0"
   def isBest(pat: Pattern[Option[Occurrence]]): Boolean = true
   def expand(f: Fringe): Option[Seq[Fringe]] = Some(Seq())
+  def contract(f: Fringe, children: Seq[Pattern[String]]): Pattern[String] = {
+    val symbol = Parser.getSymbolAtt(f.symlib.sortAtt(f.sort), "unit").get
+    f.sortInfo.category match {
+      case SetS() => SetP(Seq(), None, symbol, SymbolP(symbol, Seq()))
+      case MapS() => MapP(Seq(), Seq(), None, symbol, SymbolP(symbol, Seq()))
+    }
+  }
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(this)
 }
 
@@ -20,6 +28,7 @@ case class NonEmpty() extends Constructor {
   def name: String = ???
   def isBest(pat: Pattern[Option[Occurrence]]): Boolean = true
   def expand(f: Fringe): Option[Seq[Fringe]] = Some(Seq(f))
+  def contract(f: Fringe, children: Seq[Pattern[String]]): Pattern[String] = children(0)
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(this)
 }
 
@@ -43,6 +52,7 @@ case class HasKey(isSet: Boolean, element: SymbolOrAlias, key: Option[Pattern[Op
         }
     }
   }
+  def contract(f: Fringe, children: Seq[Pattern[String]]): Pattern[String] = ???
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(this)
 }
 
@@ -50,6 +60,15 @@ case class HasNoKey(key: Option[Pattern[Option[Occurrence]]]) extends Constructo
   def name = "0"
   def isBest(pat: Pattern[Option[Occurrence]]): Boolean = key.isDefined && pat == key.get
   def expand(f: Fringe): Option[Seq[Fringe]] = Some(Seq(f))
+  def contract(f: Fringe, children: Seq[Pattern[String]]): Pattern[String] = {
+    val child = children(0)
+    child match {
+      case MapP(keys, values, frame, ctr, orig) => 
+        MapP(WildcardP[String]() +: keys, WildcardP[String]() +: values, frame, ctr, orig)
+      case WildcardP() | VariableP(_, _) =>
+        MapP(Seq(WildcardP[String]()), Seq(WildcardP[String]()), Some(child), Parser.getSymbolAtt(f.symlib.sortAtt(f.sort), "element").get, null)
+    }
+  }
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(this)
 }
 
@@ -59,6 +78,9 @@ case class ListC(element: SymbolOrAlias, length: Int) extends Constructor {
   def expand(f: Fringe): Option[Seq[Fringe]] = {
     val sort = f.symlib.signatures(element)._1.head
     Some((0 until length).map(i => new Fringe(f.symlib, sort, Num(i, f.occurrence), false)))
+  }
+  def contract(f: Fringe, children: Seq[Pattern[String]]): Pattern[String] = {
+    ListP(children, None, Seq(), Parser.getSymbolAtt(f.symlib.sortAtt(f.sort), "element").get, null)
   }
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(this)
 }
@@ -74,6 +96,9 @@ case class SymbolC(sym: SymbolOrAlias) extends Constructor {
       Some(sorts.zipWithIndex.map(t => new Fringe(f.symlib, t._1, Num(t._2, f.occurrence), sym.ctr == "inj")))
     }
   }
+  def contract(f: Fringe, children: Seq[Pattern[String]]): Pattern[String] = {
+    SymbolP(sym, children)
+  }
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(this)
 }
 
@@ -81,5 +106,8 @@ case class LiteralC(literal: String) extends Constructor {
   def name: String = literal
   def isBest(pat: Pattern[Option[Occurrence]]): Boolean = true
   def expand(f: Fringe): Option[Seq[Fringe]] = Some(Seq())
+  def contract(f: Fringe, children: Seq[Pattern[String]]): Pattern[String] = {
+    LiteralP(literal, f.sortInfo.category)
+  }
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(this)
 }
