b59bca26ef758cabb7b0e3197d5380f0977f4a86
Dwight Guth <dwight.guth@runtimeverification.com>
2019-10-01 17:05:55 +0000

    add back HasNoKey

--- matching/src/main/scala/org/kframework/backend/llvm/matching/Matrix.scala
+++ matching/src/main/scala/org/kframework/backend/llvm/matching/Matrix.scala
@@ -120,12 +120,15 @@ class Column(val fringe: Fringe, val patterns: IndexedSeq[Pattern[String]], val 
   }
 
   def signatureForUsefulness: List[Constructor] = {
-    signatureForKey(None).filter(!_.isInstanceOf[HasNoKey]).map(c => {
-      if (!c.isInstanceOf[HasKey]) {
-        c
-      } else {
+    signatureForKey(None).map(c => {
+      if (c.isInstanceOf[HasKey]) {
         val hasKey = c.asInstanceOf[HasKey]
         HasKey(hasKey.isSet, hasKey.element, None)
+      } else if (c.isInstanceOf[HasNoKey]) {
+        val hasNoKey = c.asInstanceOf[HasNoKey]
+        HasNoKey(hasNoKey.isSet, None)
+      } else {
+        c
       }
     })
   }
