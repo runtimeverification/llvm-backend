b0c98ee7917e99d80f92735ebbc15ea93ffd0f2c
Dwight Guth <dwight.guth@runtimeverification.com>
2020-03-20 19:37:39 +0000

    add log analyzer tool for gc statistics

---
+++ tools/llvm-kompile-gc-stats/main.cpp
@@ -0,0 +1,89 @@
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <gmp.h>
+
+int main (int argc, char **argv) {
+  const char *usage = "usage: %s [dump|analyze|generation|count] <file> [<lower_bound> <upper_bound>]\n";
+  if (argc < 3) {
+    fprintf(stderr, usage, argv[0]);
+    return 1;
+  }
+  FILE *f = fopen(argv[2], "rb");
+  size_t frame[2049];
+  mpz_t total[2048];
+  size_t step = 0;
+  // `llvm-kompile-gc-stats dump` dumps the raw log to stdout. useful for
+  // debugging, less useful as information
+  bool dump = strcmp(argv[1], "dump") == 0;
+  // `llvm-kompile-gc-stats analyze` emits the total number of bytes over the
+  // life of the program that survived exactly N collections for N in [0, 2046]
+  // as well as the total number of bytes that survived >2046 collections
+  bool analyze = strcmp(argv[1], "analyze") == 0;
+  // `llvm-kompile-gc-stats generation` emits the size in bytes of a generation
+  // with a user-specified size at each step of the trace. This can then be
+  // analyzed further to determine max heap size and average heap size over
+  // the life of a program.
+  bool generation = strcmp(argv[1], "generation") == 0;
+  // `llvm-kompile-gc-stats count` emits the number of collections in the log.
+  bool count = strcmp(argv[1], "count") == 0;
+  if (analyze) {
+    for (int i = 0; i < 2048; i++) {
+      mpz_init(total[i]);
+    }
+  }
+  int lowerBound;
+  int upperBound;
+  mpz_t size;
+  if (generation) {
+    lowerBound = atoi(argv[3]);
+    upperBound = atoi(argv[4]);
+    mpz_init(size);
+  }
+  while(true) {
+    int ret = fread(frame, sizeof(size_t), 2049, f);
+    if (ret < 2049) break;
+    if (dump) {
+      printf("Collection %zd\n", step);
+      for (int i = 0; i < 2048; i++) {
+        printf("%d: %zd\n", i, frame[i]);
+      }
+      printf("saturated: %zd\n", frame[2048]);
+    } else if (analyze) {
+      for (int i = 0; i < 2048; i++) {
+        mpz_add_ui(total[i], total[i], frame[i]);
+        if (i > 0) {
+          assert(mpz_cmp_ui(total[i-1], frame[i]) >= 0);
+          mpz_sub_ui(total[i-1], total[i-1], frame[i]);
+        }
+      }
+    } else if (generation) {
+      mpz_set_ui(size, 0);
+      for (int i = lowerBound; i <= upperBound; i++) {
+        if (i == 0) {
+          mpz_add_ui(size, size, frame[0]);
+          mpz_sub_ui(size, size, frame[1]);
+        } else {
+          mpz_add_ui(size, size, frame[i]);
+        }
+      }
+      gmp_printf("%zd: %Zd\n", step, size);
+    } else if (!count) {
+      fprintf(stderr, usage, argv[0]);
+      return 1;
+    }
+    step++;
+  }
+  if (analyze) {
+    for (int i = 0; i < 2047; i++) { 
+      gmp_printf("%d: %Zd\n", i, total[i]);
+    }
+    gmp_printf("saturated: %Zd\n", total[2047]);
+  } else if (count) {
+    printf("%zd collections\n", step);
+  }
+  return 0;
+}
+
+
