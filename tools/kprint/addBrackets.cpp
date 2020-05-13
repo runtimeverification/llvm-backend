ee1785a5d11503fccac6a93379bc5657b9170b4a
Dwight Guth <dwight.guth@runtimeverification.com>
2020-05-13 16:40:51 -0500

    document and clean up interface

--- tools/kprint/addBrackets.cpp
+++ tools/kprint/addBrackets.cpp
@@ -11,7 +11,21 @@ enum Fixity {
   BARE_BOTH = 3
 };
 
-int getPosition(std::string terminals, int position) {
+/**
+ * Get the index of the Nth nonterminal within the list of all terminals and
+ * nonterminals in a production.
+ *
+ * terminals: the `terminals` attribute of a symbol, representing the pattern
+ * of which production items are terminals and which are nonterminals as a
+ * pattern of 1s and 0s.
+ *
+ * position: the index of the nonterminal to look up within the list of all
+ * nonterminals of the symbol
+ *
+ * returns: the index of the nonterminal within the list of all nonterminals
+ * and terminals of the production the symbol corresponds to.
+ */
+int getNTPositionInProd(std::string terminals, int position) {
   int terminalPos = 0;
   int ntIdx = 0;
   for (; terminalPos < terminals.size(); terminalPos++) {
@@ -25,6 +39,12 @@ int getPosition(std::string terminals, int position) {
   return terminalPos;
 }
 
+/** 
+ * Get whether a production has a terminal at a particular index.
+ *
+ * Out of bounds errors do not occur with this function: if the index is out of
+ * range, `false` is returned, indicating there is not a terminal at that index
+ */
 bool hasTerminalAtIdx(std::string terminals, int position) {
   if (position < 0 || position >= terminals.length()) {
     return false;
@@ -32,10 +52,26 @@ bool hasTerminalAtIdx(std::string terminals, int position) {
   return terminals[position] == '1';
 }
 
-Fixity getFixity(int position, KORECompositePattern *outer, PrettyPrintData const& data) {
+/**
+ * Compute whether there are terminals immediately to the left and right of a
+ * nonterminal in a production.
+ *
+ * position: the index of the nonterminal to look up within the list of all
+ * nonterminals in a symbol
+ *
+ * sym: the symbol to examine
+ *
+ * data: pretty printing metadata
+ *
+ * returns: EMPTY if there are terminals on both sides of this nonterminal
+ *          BARE_LEFT if there is a terminal only on the right of this nonterminal
+ *          BARE_LEFT if there is a terminal only on the left of this nonterminal
+ *          BARE_BOTH if there are not terminals on either side of this nonterminal
+ */
+Fixity getFixity(int position, KORESymbol *sym, PrettyPrintData const& data) {
   int result = EMPTY;
-  std::string terminals = data.terminals.at(outer->getConstructor()->getName());
-  int terminalPos = getPosition(terminals, position);
+  std::string terminals = data.terminals.at(sym->getName());
+  int terminalPos = getNTPositionInProd(terminals, position);
   if (!hasTerminalAtIdx(terminals, terminalPos+1)) {
     result |= BARE_RIGHT;
   }
@@ -45,8 +81,21 @@ Fixity getFixity(int position, KORECompositePattern *outer, PrettyPrintData cons
   return (Fixity) result;
 }
 
-Fixity getFixity(KORECompositePattern *outer, PrettyPrintData const& data) {
-  std::string name = outer->getConstructor()->getName();
+/**
+ * Compute whether there are terminals on the far left and far right of a
+ * production.
+ *
+ * sym: the symbol to examine
+ *
+ * data: pretty printing metadata
+ *
+ * returns: EMPTY if there are terminals on both edges of this production.
+ *          BARE_LEFT if there is a terminal only on the right edge of this production
+ *          BARE_RIGHT if there is a terminal only on the left edge of this production
+ *          BARE_BOTH if there are not terminals on either edge of this production
+ */
+Fixity getFixity(KORESymbol *sym, PrettyPrintData const& data) {
+  std::string name = sym->getName();
   int result = EMPTY;
   if (data.terminals.at(name)[0] == '0') {
     result = result | BARE_LEFT;
@@ -57,8 +106,30 @@ Fixity getFixity(KORECompositePattern *outer, PrettyPrintData const& data) {
   return (Fixity)result;
 }
 
+/**
+ * Compute the left capture of a pattern.
+ *
+ * If you were to unparse the AST of a term without any parentheses as a
+ * sequence of terminals, start from the first terminal that is part of a
+ * particular pattern (either directly or nested) and proceed left until you
+ * reach the first terminal that is part of a pattern (directly) that is a
+ * parent of that pattern, the pattern whose terminal you reach would be the
+ * left capture of that pattern. Not all terms have a left capture; for
+ * example, the left capture of the top term in an AST does not exist.
+ *
+ * previousLeftCapture: the left capture of `outer`
+ *
+ * outer: the parent of the symbol you wish to compute the left capture of
+ *
+ * position: the position within the nonterminals of `outer` of the pattern
+ * you wish to compute the left capture of.
+ *
+ * data: pretty printing metadata
+ *
+ * returns: the left capture of the term, or NULL if no such term exists
+ */ 
 KORECompositePattern *getLeftCapture(KORECompositePattern *previousLeftCapture, KORECompositePattern *outer, int position, PrettyPrintData const& data) {
-  Fixity fixity = getFixity(outer, data);
+  Fixity fixity = getFixity(outer->getConstructor(), data);
   if (position == 0 && (fixity & BARE_LEFT)) {
     return previousLeftCapture;
   } else {
@@ -66,8 +137,31 @@ KORECompositePattern *getLeftCapture(KORECompositePattern *previousLeftCapture, 
   }
 }
 
+/**
+ * Compute the right capture of a pattern.
+ *
+ * If you were to unparse the AST of a term without any parentheses as a
+ * sequence of terminals, start from the last terminal that is part of a
+ * particular pattern (either directly or nested) and proceed right until you
+ * reach the first terminal that is part of a pattern (directly) that is a
+ * parent of that pattern, the pattern whose terminal you reach would be the
+ * right capture of that pattern. Not all terms have a right capture; for
+ * example, the right capture of the top term in an AST does not exist.
+ *
+ * previousRightCapture: the right capture of `outer`
+ *
+ * outer: the parent of the symbol you wish to compute the right capture of
+ *
+ * position: the position within the nonterminals of `outer` of the pattern
+ * you wish to compute the right capture of.
+ *
+ * data: pretty printing metadata
+ *
+ * returns: the right capture of the term, or NULL if no such term exists
+ */ 
+
 KORECompositePattern *getRightCapture(KORECompositePattern *previousRightCapture, KORECompositePattern *outer, int position, PrettyPrintData const& data) {
-  Fixity fixity = getFixity(outer, data);
+  Fixity fixity = getFixity(outer->getConstructor(), data);
   if (position == outer->getArguments().size() - 1 && (fixity & BARE_RIGHT)) {
     return previousRightCapture;
   } else {
@@ -75,10 +169,39 @@ KORECompositePattern *getRightCapture(KORECompositePattern *previousRightCapture
   }
 }
 
+/**
+ * Computes whether one sort is less than or equal to the other according to
+ * the subsort relation of the definition.
+ *
+ * data: pretty printing metadata
+ *
+ * s1: first sort
+ *
+ * s2: second sort
+ *
+ * returns: s1 <= s2
+ */
 bool lessThanEq(PrettyPrintData const& data, KORESort *s1, KORESort *s2) {
   return *s1 == *s2 || (data.subsorts.count(s1) && data.subsorts.at(s1).count(s2));
 }
 
+/**
+ * Computes whether `inner` can appear at position `position` inside `outer`
+ * according to the rules of subsorting, priority, and associativity. The
+ * pattern does not actually have to exist at that position; we ask only
+ * whether it `can` appear there.
+ *
+ * outer: the outer pattern
+ *
+ * inner: the inner pattern
+ *
+ * position: a position within `outer` you wish to test
+ *
+ * data: pretty printing metadata
+ *
+ * returns: true if priority forbids `inner` to appear inside `outer` at that
+ * position.
+ */
 bool isPriorityWrong(KORECompositePattern *outer, KORECompositePattern *inner, int position, PrettyPrintData const& data) {
   std::string outerName = outer->getConstructor()->getName();
   std::string innerName = inner->getConstructor()->getName();
@@ -91,7 +214,7 @@ bool isPriorityWrong(KORECompositePattern *outer, KORECompositePattern *inner, i
     return true;
   }
   std::string terminals = data.terminals.at(outerName);
-  int terminalPos = getPosition(terminals, position);
+  int terminalPos = getNTPositionInProd(terminals, position);
   if (data.leftAssoc.count(outerName) && data.leftAssoc.at(outerName).count(innerName) && terminalPos == terminals.size() - 1) {
     return true;
   }
@@ -101,6 +224,40 @@ bool isPriorityWrong(KORECompositePattern *outer, KORECompositePattern *inner, i
   return false;
 }
 
+/**
+ * Computes whether a bracket needs to appear around `inner`.
+ *
+ * outer: the parent of `inner`
+ *
+ * leftCapture: the left capture of `inner`
+ *
+ * rightCapture: the right capture of `inner`
+ *
+ * inner: the pattern to test
+ *
+ * position: the position of `inner` within the nonterminals of `outer`
+ *
+ * data: pretty printing metadata
+ *
+ * returns: true if a bracket is required according to this algorithm. Roughly
+ * speaking, parentheses are not needed around associative symbols, constants,
+ * or patterns that appear between two terminals in their parent. Otherwise,
+ * parentheses are needed if priority or associativity forbids the term inside
+ * its parent, or if the term begins or ends with a nonterminal and priority
+ * and associativity do not forbid the term from appearing inside its left
+ * or right capture, respectively, and its left or right capture does not
+ * end or begin, respectively, with a terminal.
+ *
+ * Warning: this algorithm is *unsound*. There is no guarantee that after 
+ * it completes, the resulting term can be unparsed into a string that will
+ * parse unambiguously as the original AST. Indeed, the problem of inserting
+ * brackets into an AST in order to ensure this property is NP-hard, and 
+ * grammars can be constructed such that solving the unparsing problem is 
+ * equivalent to boolean satisfiability. This algorithm is merely a linear-time
+ * approximation of the general problem which performs very well in most
+ * real-world grammars, especially the common expression-grammar category of
+ * grammar where parentheses are most often required in practice.
+ */
 bool requiresBracketWithSimpleAlgorithm(KORECompositePattern *outer, KORECompositePattern *leftCapture, KORECompositePattern *rightCapture, KOREPattern *inner, int position, PrettyPrintData const& data) {
   if (auto innerComposite = dynamic_cast<KORECompositePattern *>(inner)) {
     std::string innerName = innerComposite->getConstructor()->getName();
@@ -112,7 +269,7 @@ bool requiresBracketWithSimpleAlgorithm(KORECompositePattern *outer, KOREComposi
     if (innerName == "\\dv") {
       return false;
     }
-    Fixity fixity = getFixity(position, outer, data);
+    Fixity fixity = getFixity(position, outer->getConstructor(), data);
     if (fixity == EMPTY) {
       return false;
     }
@@ -123,17 +280,17 @@ bool requiresBracketWithSimpleAlgorithm(KORECompositePattern *outer, KOREComposi
     if (data.terminals.at(innerName) == "0") {
       return false;
     }
-    Fixity innerFixity = getFixity(innerComposite, data);
+    Fixity innerFixity = getFixity(innerComposite->getConstructor(), data);
     if ((innerFixity & BARE_RIGHT) && rightCapture != nullptr) {
       bool inversePriority = isPriorityWrong(innerComposite, rightCapture, innerComposite->getArguments().size() - 1, data);
-      Fixity rightCaptureFixity = getFixity(rightCapture, data);
+      Fixity rightCaptureFixity = getFixity(rightCapture->getConstructor(), data);
       if (!inversePriority && (rightCaptureFixity & BARE_LEFT)) {
         return true;
       }
     }
     if ((innerFixity & BARE_LEFT) && leftCapture != nullptr) {
       bool inversePriority = isPriorityWrong(innerComposite, leftCapture, 0, data);
-      Fixity leftCaptureFixity = getFixity(leftCapture, data);
+      Fixity leftCaptureFixity = getFixity(leftCapture->getConstructor(), data);
       if (!inversePriority && (leftCaptureFixity & BARE_RIGHT)) {
         return true;
       }
