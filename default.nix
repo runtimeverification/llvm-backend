680a45d02a1b8150131a9bf3c7a164a7cfad9bb0
Thomas Tuegel <ttuegel@mailbox.org>
2020-11-05 11:31:01 -0600

    Move clang configuration to default.nix

--- default.nix
+++ default.nix
@@ -19,8 +19,38 @@ let
     inherit mavenix;
   };
 
+  clang =
+    let
+      inherit (pkgs) stdenv mkShell wrapCCWith wrapBintoolsWith;
+      llvmPackages = pkgs.llvmPackages_10;
+      inherit (llvmPackages) tools libraries;
+      mkExtraBuildCommands = cc: ''
+        rsrc="$out/resource-root"
+        mkdir "$rsrc"
+        ln -s "${cc}/lib/clang/10.0.0/include" "$rsrc"
+        ln -s "${libraries.compiler-rt.out}/lib" "$rsrc/lib"
+        echo "-resource-dir=$rsrc" >> $out/nix-support/cc-cflags
+      '' + stdenv.lib.optionalString (stdenv.targetPlatform.isLinux && !(stdenv.targetPlatform.useLLVM or false)) ''
+        echo "--gcc-toolchain=${tools.clang-unwrapped.gcc}" >> $out/nix-support/cc-cflags
+      '';
+    in
+    wrapCCWith rec {
+      cc = tools.clang-unwrapped;
+      libcxx = null;
+      bintools = wrapBintoolsWith {
+        inherit (tools) bintools;
+      };
+      extraPackages = [
+        libraries.compiler-rt
+      ];
+      extraBuildCommands = ''
+        echo "-rtlib=compiler-rt -Wno-unused-command-line-argument" >> $out/nix-support/cc-cflags
+        echo "-B${libraries.compiler-rt}/lib" >> $out/nix-support/cc-cflags
+      '' + mkExtraBuildCommands cc;
+    };
+
   self = {
-    inherit llvm-backend llvm-backend-matching;
+    inherit clang llvm-backend llvm-backend-matching;
     inherit mavenix;
   };
 
--- test.nix
+++ test.nix
@@ -6,40 +6,12 @@ in
 { pkgs ? pinned }:
 
 let
-  inherit (pkgs) stdenv mkShell wrapCCWith wrapBintoolsWith;
-  llvmPackages = pkgs.llvmPackages_10;
-  clang =
-    let
-      inherit (llvmPackages) tools libraries;
-      mkExtraBuildCommands = cc: ''
-        rsrc="$out/resource-root"
-        mkdir "$rsrc"
-        ln -s "${cc}/lib/clang/10.0.0/include" "$rsrc"
-        ln -s "${libraries.compiler-rt.out}/lib" "$rsrc/lib"
-        echo "-resource-dir=$rsrc" >> $out/nix-support/cc-cflags
-      '' + stdenv.lib.optionalString (stdenv.targetPlatform.isLinux && !(stdenv.targetPlatform.useLLVM or false)) ''
-        echo "--gcc-toolchain=${tools.clang-unwrapped.gcc}" >> $out/nix-support/cc-cflags
-      '';
-    in
-    wrapCCWith rec {
-      cc = tools.clang-unwrapped;
-      libcxx = null;
-      bintools = wrapBintoolsWith {
-        inherit (tools) bintools;
-      };
-      extraPackages = [
-        libraries.compiler-rt
-      ];
-      extraBuildCommands = ''
-        echo "-rtlib=compiler-rt -Wno-unused-command-line-argument" >> $out/nix-support/cc-cflags
-        echo "-B${libraries.compiler-rt}/lib" >> $out/nix-support/cc-cflags
-      '' + mkExtraBuildCommands cc;
-    };
+  inherit (pkgs) mkShell;
 
   inherit (pkgs) coreutils diffutils git ncurses gmp mpfr libffi jemalloc;
 
   default = import ./. { inherit pkgs; };
-  inherit (default) llvm-backend llvm-backend-matching;
+  inherit (default) clang llvm-backend llvm-backend-matching;
 
 in mkShell {
   inputsFrom = [
