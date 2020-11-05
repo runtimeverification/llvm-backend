a7afed48b48beaf91af46f57e9be0ccd0350d0e1
Thomas Tuegel <ttuegel@mailbox.org>
2020-11-05 13:24:52 -0600

    test.nix: Run integration tests in Nix builder

--- test.nix
+++ test.nix
@@ -6,21 +6,40 @@ in
 { pkgs ? pinned }:
 
 let
-  inherit (pkgs) mkShell;
-
+  inherit (pkgs) stdenv;
   inherit (pkgs) coreutils diffutils git ncurses gmp mpfr libffi jemalloc;
 
   default = import ./. { inherit pkgs; };
-  inherit (default) clang llvm-backend llvm-backend-matching;
+  inherit (default) clang llvm-backend llvm-backend-matching llvm-kompile-testing;
 
-in mkShell {
-  inputsFrom = [
-    llvm-backend-matching
-  ];
+in
 
+stdenv.mkDerivation {
+  name = "llvm-backend-test";
+  src = llvm-backend.src;
+  preferLocalBuild = true;
   buildInputs = [
-    coreutils diffutils git ncurses gmp mpfr libffi jemalloc
-    clang
-    llvm-backend
+    diffutils git ncurses gmp mpfr libffi jemalloc
+    clang llvm-backend llvm-kompile-testing
   ];
+  configurePhase = "true";
+  buildPhase = ''
+    runHook preBuild
+
+    mkdir -p build; cd build
+    cp ../test/Makefile .
+    make KOMPILE=llvm-kompile-testing clean
+    make KOMPILE=llvm-kompile-testing -O -j$NIX_MAX_JOBS
+
+    runHook postBuild
+  '';
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out"
+    cp -a -t "$out" .
+
+    runHook postInstall
+  '';
 }
+
