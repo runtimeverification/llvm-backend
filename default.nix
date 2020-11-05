563504d5e0f803565cd45faae46f24b48efdaeb9
Thomas Tuegel <ttuegel@mailbox.org>
2020-11-05 11:41:04 -0600

    default.nix: Clean up clang configuration

--- default.nix
+++ default.nix
@@ -19,35 +19,20 @@ let
     inherit mavenix;
   };
 
+  # The backend requires clang/lld/libstdc++ at runtime.
+  # The closest configuration in Nixpkgs is clang/lld without any C++ standard
+  # library. We override that configuration to inherit libstdc++ from stdenv.
   clang =
     let
-      inherit (pkgs) stdenv mkShell wrapCCWith wrapBintoolsWith;
       llvmPackages = pkgs.llvmPackages_10;
-      inherit (llvmPackages) tools libraries;
-      mkExtraBuildCommands = cc: ''
-        rsrc="$out/resource-root"
-        mkdir "$rsrc"
-        ln -s "${cc}/lib/clang/10.0.0/include" "$rsrc"
-        ln -s "${libraries.compiler-rt.out}/lib" "$rsrc/lib"
-        echo "-resource-dir=$rsrc" >> $out/nix-support/cc-cflags
-      '' + stdenv.lib.optionalString (stdenv.targetPlatform.isLinux && !(stdenv.targetPlatform.useLLVM or false)) ''
-        echo "--gcc-toolchain=${tools.clang-unwrapped.gcc}" >> $out/nix-support/cc-cflags
-      '';
-    in
-    wrapCCWith rec {
-      cc = tools.clang-unwrapped;
-      libcxx = null;
-      bintools = wrapBintoolsWith {
-        inherit (tools) bintools;
+      override = attrs: {
+        extraBuildCommands = ''
+          ${attrs.extraBuildCommands}
+          sed -i $out/nix-support/cc-cflags -e '/^-nostdlib/ d'
+        '';
       };
-      extraPackages = [
-        libraries.compiler-rt
-      ];
-      extraBuildCommands = ''
-        echo "-rtlib=compiler-rt -Wno-unused-command-line-argument" >> $out/nix-support/cc-cflags
-        echo "-B${libraries.compiler-rt}/lib" >> $out/nix-support/cc-cflags
-      '' + mkExtraBuildCommands cc;
-    };
+    in
+      llvmPackages.lldClangNoLibcxx.override override;
 
   self = {
     inherit clang llvm-backend llvm-backend-matching;
