a045715895f4dd0f120c3aff98ecff3340a83fae
Thomas Tuegel <ttuegel@mailbox.org>
2021-01-30 10:23:27 -0600

    Update update-maven.sh to best practices

--- nix/update-maven.sh
+++ nix/update-maven.sh
@@ -4,4 +4,12 @@
 # Run this script (from the top level of the repository) when the maven
 # project's dependencies change.
 
+# Ensure the source is loaded into the Nix store.
+# This command will fail, but only after loading the source.
+# mavenix will not do this automatically because we it uses restrict-eval,
+# and we are using filterSource, which is disabled under restrict-eval.
+nix-build --no-out-link \
+    -E '(import ./default.nix {}).llvm-backend-matching.src' \
+    || echo "^~~~ expected error"
+
 mvnix-update -E '(import ./default.nix {}).llvm-backend-matching'
