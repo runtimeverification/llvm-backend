e7666085abcabeb883d7b3d2a094647d8588e49a
Jan Tusil <jenda.tusil@gmail.com>
2021-02-25 16:11:08 +0100

    Fix stack overflow when destructing the parsed KORE

--- lib/ast/AST.cpp
+++ lib/ast/AST.cpp
@@ -1658,3 +1658,23 @@ void kllvm::readMultimap(std::string name, KORESymbolDeclaration *decl, std::map
     }
   }
 }
+
+// Normally, destruction of KOREPattern would call destructor
+// of all its subpatterns. This can sometimes exhaust all the stack space.
+// This function deallocates a pattern iteratively, without recursion.
+void kllvm::deallocateSPtrKorePattern(sptr<KOREPattern> pattern) {
+    std::vector<sptr<KOREPattern>> vec;
+    vec.push_back(std::move(pattern));
+    while(!vec.empty()) {
+        sptr<KOREPattern> curr = std::move(vec.back());
+        vec.pop_back();
+        if (auto composite = std::dynamic_pointer_cast<KORECompositePattern>(curr)) {
+            vec.reserve(vec.size() + composite->arguments.size());
+            // move all the arguments to vec
+            while(!composite->arguments.empty()) {
+                vec.push_back(std::move(composite->arguments.back()));
+                composite->arguments.pop_back();
+            }
+        }
+    }
+}
