79524aa6bd14691e853382cbdf68936ab03ba0f5
Bruce Collie <brucecollie82@gmail.com>
2021-11-02 14:13:15 +0000

    Don't rely on bare llvm-config

--- cmake/SetTriple.cmake
+++ cmake/SetTriple.cmake
@@ -1,11 +1,6 @@
 execute_process(
-  COMMAND llvm-config --host-target
+  COMMAND ${LLVM_TOOLS_BINARY_DIR}/llvm-config --host-target
   OUTPUT_VARIABLE BACKEND_TARGET_TRIPLE
-  OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-execute_process(
-  COMMAND llvm-config --bindir
-  OUTPUT_VARIABLE LLVM_BIN_DIR
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 # This is the best way I can figure out to get a _working_ default datalayout
@@ -16,7 +11,7 @@ execute_process(
 # clang, and figures out what the _frontend_ thinks the datalayout should be by
 # scraping it from the generated LLVM.
 execute_process(
-  COMMAND "${CMAKE_SOURCE_DIR}/cmake/guess-triple.sh" "${LLVM_BIN_DIR}/clang"
+  COMMAND "${CMAKE_SOURCE_DIR}/cmake/guess-triple.sh" "${LLVM_TOOLS_BINARY_DIR}/clang"
   OUTPUT_VARIABLE BACKEND_TARGET_DATALAYOUT
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 
