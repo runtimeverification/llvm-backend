b460bd5788f30ebb02dffd845de05b52049b4cfe
Bruce Collie <brucecollie82@gmail.com>
2021-11-03 12:46:12 +0000

    Fix build on Nix

--- cmake/SetTriple.cmake
+++ cmake/SetTriple.cmake
@@ -1,7 +1,15 @@
+if(NOT LLVM_CONFIG_PATH)
+  set(LLVM_CONFIG_PATH ${LLVM_TOOLS_BINARY_DIR}/llvm-config)
+endif()
+
 execute_process(
-  COMMAND ${LLVM_TOOLS_BINARY_DIR}/llvm-config --host-target
+  COMMAND ${LLVM_CONFIG_PATH} --host-target
   OUTPUT_VARIABLE BACKEND_TARGET_TRIPLE
   OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+if(NOT LLVM_CLANG_PATH)
+  set(LLVM_CLANG_PATH "${LLVM_TOOLS_BINARY_DIR}/clang")
+endif()
 
 # This is the best way I can figure out to get a _working_ default datalayout
 # for the host platform. If it's left blank (and the LLVM internals supply one),
@@ -11,8 +19,10 @@ execute_process(
 # clang, and figures out what the _frontend_ thinks the datalayout should be by
 # scraping it from the generated LLVM.
 execute_process(
-  COMMAND "${CMAKE_SOURCE_DIR}/cmake/guess-triple.sh" "${LLVM_TOOLS_BINARY_DIR}/clang"
+  COMMAND "sh" "${CMAKE_SOURCE_DIR}/cmake/guess-triple.sh" "${LLVM_CLANG_PATH}"
+  COMMAND_ECHO STDOUT
   OUTPUT_VARIABLE BACKEND_TARGET_DATALAYOUT
+  COMMAND_ERROR_IS_FATAL ANY
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 message(STATUS "Using LLVM datalayout: ${BACKEND_TARGET_DATALAYOUT}")
