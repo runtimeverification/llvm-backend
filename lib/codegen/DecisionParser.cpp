4d78f8b93f3f1a5133c9f1baef1084db32647775
Dwight Guth <dwight.guth@runtimeverification.com>
2021-12-16 14:35:00 -0600

    base64 decode bytes literals in DecisionParser.cpp

--- lib/codegen/DecisionParser.cpp
+++ lib/codegen/DecisionParser.cpp
@@ -1,4 +1,5 @@
 #include "kllvm/codegen/DecisionParser.h"
+#include "base64.h"
 #include "kllvm/codegen/CreateTerm.h"
 #include "kllvm/codegen/Decision.h"
 #include "kllvm/codegen/Util.h"
@@ -154,9 +155,13 @@ public:
       auto sym = KORESymbol::Create("\\dv");
       auto hook = str(get(node, "hook"));
       auto sort = sorts.at(KORECompositeSort::getCategory(hook));
-      auto val = str(get(node, "literal"));
+      auto lit = get(node, "literal");
+      auto val = str(lit);
       if (hook == "BOOL.Bool") {
         val = val == "1" ? "true" : "false";
+      }
+      if (std::string((char *)lit->tag) == "tag:yaml.org,2002:binary") {
+        val = base64_decode(val);
       }
 
       sym->addFormalArgument(sort);
