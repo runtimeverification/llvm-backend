61b73d1d6a0da9132a2c298c7cf5a7b986f69010
Bruce Collie <brucecollie82@gmail.com>
2022-03-07 16:37:34 +0000

    Call unimplemented function

--- runtime/util/ConfigurationSerializer.cpp
+++ runtime/util/ConfigurationSerializer.cpp
@@ -1,1 +1,9 @@
+#include <kllvm/binary/serializer.h>
 
+#include "runtime/header.h"
+
+#include <cassert>
+
+void serializeConfiguration(const char *filename, block *subject) {
+  assert(false && "Unimplemented");
+}
