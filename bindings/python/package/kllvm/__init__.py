1eb34be149397fca9897a04214653cbfdf1ec7d4
Bruce Collie <brucecollie82@gmail.com>
2022-09-22 11:05:44 +0100

    Minimal glue between ast + runtime

--- bindings/python/package/kllvm/__init__.py
+++ bindings/python/package/kllvm/__init__.py
@@ -1,1 +1,12 @@
 from ._kllvm import *
+
+import importlib
+import sys
+
+
+def load_definition(path):
+    global runtime
+    old_path = sys.path
+    sys.path = [path]
+    runtime = importlib.import_module("_kllvm_runtime")
+    sys.path = old_path
--- bindings/python/runtime.cpp
+++ bindings/python/runtime.cpp
@@ -1,11 +1,57 @@
+#include <kllvm/ast/AST.h>
+
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
-void bind_runtime(py::module_ &m) {
-  auto runtime = m.def_submodule("runtime", "K LLVM backend runtime");
+using namespace kllvm;
+
+/*
+ * We can't use the pybind default holders because they'll try to take ownership
+ * of the runtime's objects. This is the minimum viable holder that does _not_
+ * express ownership of the underlying object.
+ */
+template <typename T>
+struct raw_ptr {
+  T *ptr_;
+  T *get() { return ptr_; }
+  raw_ptr(T *ptr)
+      : ptr_(ptr) { }
+};
+PYBIND11_DECLARE_HOLDER_TYPE(T, raw_ptr<T>, true);
+
+// TODO:  fix name clashes with macros in header.h so that we don't need to slice
+//        these ones out.
+extern "C" {
+typedef struct blockheader {
+  uint64_t hdr;
+} blockheader;
+
+typedef struct block {
+  blockheader h;
+  uint64_t *children[];
+} block;
+
+/* block *parseConfiguration(const char *filename); */
+
+/* block *take_steps(int64_t, block *); */
+/* void finish_rewriting(block *, bool); */
+/* void printConfiguration(const char *filename, block *subject); */
+/* void initStaticObjects(); */
 }
 
-PYBIND11_MODULE(kllvm_runtime, m) {
+void *constructInitialConfiguration(const KOREPattern *initial);
+static block *construct_stub(const KOREPattern *initial) {
+  return static_cast<block *>(constructInitialConfiguration(initial));
+}
+
+void bind_runtime(py::module_ &m) {
+  auto runtime = m.def_submodule("runtime", "K LLVM backend runtime");
+
+  py::class_<block, raw_ptr<block>>(m, "Term").def(
+      py::init([](KOREPattern const *init) { return construct_stub(init); }));
+}
+
+PYBIND11_MODULE(_kllvm_runtime, m) {
   bind_runtime(m);
 }
