2aa0efdd777cc5d76bb8f5ac16b4a9f739d35120
Bruce Collie <brucecollie82@gmail.com>
2022-09-28 09:30:30 +0100

    Runtime binding features (#568)
    * Improve interface part 1
    
    * Use new script in tests
    
    * Build python module
    
    * Change name to runtime
    
    * Add basic loading test
    
    * Fix ignore file
    
    * Fix up nix
    
    * Test existing sort bindings
    
    * Restructure python for future
    
    * Start to bring in symbols
    
    * Metaprogramming nonsense
    
    * Add adapter for symbol with params
    
    * Add sorts
    
    * Finish up symbols
    
    * Working on patterns
    
    * Finish up patterns
    
    * Start parser
    
    * Fix parser ownership
    
    * Minimal glue between ast + runtime
    
    * Add first basic runtime test
    
    * Fix tests
    
    * Whitespace
    
    * Test printing terms, no step
    
    * Rename step function
    
    * Implement and test taking steps
    
    * We can use the header
    
    * Test macos fixes£
    
    * Drop visibility
    
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

--- bindings/python/runtime.cpp
+++ bindings/python/runtime.cpp
@@ -1,11 +1,68 @@
+#include <kllvm/ast/AST.h>
+
 #include <pybind11/pybind11.h>
+
+// This header needs to be included last because it pollutes a number of macro
+// definitions into the global namespace.
+#include <runtime/header.h>
 
 namespace py = pybind11;
 
-void bind_runtime(py::module_ &m) {
-  auto runtime = m.def_submodule("runtime", "K LLVM backend runtime");
+using namespace kllvm;
+
+/*
+ * We can't use the pybind default holders because they'll try to take ownership
+ * of the runtime's objects. This is the minimum viable holder that does _not_
+ * express ownership of the underlying object.
+ */
+template <typename T>
+class raw_ptr {
+public:
+  explicit raw_ptr(T *ptr)
+      : ptr_(ptr) { }
+
+  raw_ptr()
+      : raw_ptr(nullptr) { }
+
+  T *get() { return ptr_; }
+
+private:
+  T *ptr_;
+};
+
+PYBIND11_DECLARE_HOLDER_TYPE(T, raw_ptr<T>, true);
+
+/*
+ * These declarations are internal to the backend and aren't exposed explicitly
+ * through any header files, so we pull them in manually here.
+ */
+
+extern "C" {
+void initStaticObjects();
+block *take_steps(int64_t, block *);
 }
 
-PYBIND11_MODULE(kllvm_runtime, m) {
+void *constructInitialConfiguration(const KOREPattern *initial);
+
+void bind_runtime(py::module_ &m) {
+  auto runtime = m.def_submodule("runtime", "K LLVM backend runtime");
+
+  // This class can't be used directly from Python; the mutability semantics
+  // that we get from the Pybind wrappers make it really easy to break things.
+  // We therefore have to wrap it up in some external Python code; see
+  // package/kllvm/__init__.py for the details of the external class.
+  py::class_<block, raw_ptr<block>>(m, "InternalTerm")
+      .def(py::init([](KOREPattern const *init) {
+        return static_cast<block *>(constructInitialConfiguration(init));
+      }))
+      .def(
+          "__str__",
+          [](block *term) { return printConfigurationToString(term)->data; })
+      .def("step", [](block *term, int64_t n) { return take_steps(n, term); });
+}
+
+PYBIND11_MODULE(_kllvm_runtime, m) {
+  initStaticObjects();
+
   bind_runtime(m);
 }
--- bindings/python/runtime.h
+++
@@ -1,6 +0,0 @@
-#ifndef KLLVM_PYTHON_RUNTIME_H
-#define KLLVM_PYTHON_RUNTIME_H
-
-void bind_runtime(pybind11::module_);
-
-#endif
