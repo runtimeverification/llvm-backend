1cf37eee33c1ccf9c49f501900bde93471f6be38
Bruce Collie <brucecollie82@gmail.com>
2022-11-16 12:25:55 +0000

    GC?

--- flake.nix
+++ flake.nix
@@ -115,10 +115,10 @@
           llvm-backend-13-Release.llvm-backend
 
           llvm-backend-14-Debug.integration-tests
-#           llvm-backend-14-Release.integration-tests
-#           llvm-backend-14-RelWithDebInfo.integration-tests
-#           llvm-backend-14-FastBuild.integration-tests
-#           llvm-backend-14-GcStats.integration-tests
+          llvm-backend-14-Release.integration-tests
+          llvm-backend-14-RelWithDebInfo.integration-tests
+          llvm-backend-14-FastBuild.integration-tests
+          llvm-backend-14-GcStats.integration-tests
         ];
         devShells.default = llvm-backend-14-FastBuild.devShell;
       }) // {
