5d008e1533b6d396975ffb46ad4a3e1992475d85
Bruce Collie <brucecollie82@gmail.com>
2022-11-28 11:05:09 +0000

    Migrate createterm

--- lib/codegen/CreateTerm.cpp
+++ lib/codegen/CreateTerm.cpp
@@ -537,7 +537,7 @@ llvm::Value *CreateTerm::createHook(
       return result;
     } else if (nwords == 1) {
       auto Word = new llvm::LoadInst(
-          Ptr->getType()->getPointerElementType(), Ptr, "word", CurrentBlock);
+          llvm::Type::getInt64Ty(Ctx), Ptr, "word", CurrentBlock);
       if (cat.bits == 64) {
         return Word;
       } else {
@@ -547,7 +547,7 @@ llvm::Value *CreateTerm::createHook(
     } else { // nwords >= 2
       for (size_t i = 0; i < nwords; i++) {
         auto Word = new llvm::LoadInst(
-            Ptr->getType()->getPointerElementType(), Ptr, "word", CurrentBlock);
+            llvm::Type::getInt64Ty(Ctx), Ptr, "word", CurrentBlock);
         auto Zext = new llvm::ZExtInst(Word, Type, "extended", CurrentBlock);
         auto Shl = llvm::BinaryOperator::Create(
             llvm::Instruction::Shl, result, llvm::ConstantInt::get(Type, 64),
@@ -858,8 +858,7 @@ llvm::Value *CreateTerm::notInjectionCase(
         = llvm::PointerType::get(BlockType->elements()[idx], 0);
     if (ChildValue->getType() == ChildPtrType) {
       ChildValue = new llvm::LoadInst(
-          ChildValue->getType()->getPointerElementType(), ChildValue, "",
-          CurrentBlock);
+          BlockType->elements()[idx], ChildValue, "", CurrentBlock);
     }
     children.push_back(ChildValue);
     idx++;
