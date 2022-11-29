33ac7dbd53e5a8304c78268137f3093d98572d92
Bruce Collie <brucecollie82@gmail.com>
2022-11-29 14:12:59 +0000

    FFI for pattern

---
+++ bindings/c/include/kllvm-c/kllvm-c.h
@@ -0,0 +1,13 @@
+#ifndef KLLVM_C_H
+#define KLLVM_C_H
+
+extern "C" {
+
+typedef struct kore_composite_pattern kore_composite_pattern;
+
+kore_composite_pattern *kore_composite_pattern_new(char const *);
+void kore_composite_pattern_dump(kore_composite_pattern const *);
+void kore_composite_pattern_free(kore_composite_pattern const *);
+}
+
+#endif
--- bindings/c/lib.cpp
+++ bindings/c/lib.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <memory>
 
-extern "C" bool hello(char const *name) {
-  std::cout << "Hello, " << name << "!\n";
-  return true;
+#include <kllvm/ast/AST.h>
+
+#include <kllvm-c/kllvm-c.h>
+
+extern "C" {
+
+struct kore_composite_pattern {
+  std::unique_ptr<kllvm::KORECompositePattern> ptr_;
+};
+
+kore_composite_pattern *kore_composite_pattern_new(char const *name) {
+  auto pat = new kore_composite_pattern;
+  pat->ptr_ = kllvm::KORECompositePattern::Create(std::string(name));
+  return pat;
+}
+
+void kore_composite_pattern_dump(kore_composite_pattern const *pat) {
+  pat->ptr_->print(std::cout);
+}
+
+void kore_composite_pattern_free(kore_composite_pattern const *pat) {
+  delete pat;
+}
 }
