5f4382da85640749b111a027e60a948aacd27312
Bruce Collie <brucecollie82@gmail.com>
2022-12-01 15:21:56 +0000

    Starting on polymorphism

--- bindings/c/include/kllvm-c/kllvm-c.h
+++ bindings/c/include/kllvm-c/kllvm-c.h
@@ -3,13 +3,13 @@
 
 extern "C" {
 
-typedef struct kore_composite_pattern kore_composite_pattern;
+typedef struct kore_pattern kore_pattern;
 
-kore_composite_pattern *kore_composite_pattern_new(char const *);
-void kore_composite_pattern_dump(kore_composite_pattern const *);
-void kore_composite_pattern_add_argument(
-    kore_composite_pattern *, kore_composite_pattern *);
-void kore_composite_pattern_free(kore_composite_pattern const *);
+void kore_pattern_dump(kore_pattern const *);
+void kore_pattern_free(kore_pattern const *);
+
+kore_pattern *kore_composite_pattern_new(char const *);
+void kore_composite_pattern_add_argument(kore_pattern *, kore_pattern *);
 }
 
 #endif
--- bindings/c/lib.cpp
+++ bindings/c/lib.cpp
@@ -7,27 +7,43 @@
 
 extern "C" {
 
-struct kore_composite_pattern {
-  std::unique_ptr<kllvm::KORECompositePattern> ptr_;
+struct kore_pattern {
+  std::unique_ptr<kllvm::KOREPattern> ptr_;
 };
 
-kore_composite_pattern *kore_composite_pattern_new(char const *name) {
-  auto pat = new kore_composite_pattern;
+/* KOREPattern */
+
+void kore_pattern_dump(kore_pattern const *pat) {
+  pat->ptr_->print(std::cout);
+}
+
+void kore_pattern_free(kore_pattern const *pat) {
+  delete pat;
+}
+
+/* KORECompositePattern */
+
+kore_pattern *kore_composite_pattern_new(char const *name) {
+  auto pat = new kore_pattern;
   pat->ptr_ = kllvm::KORECompositePattern::Create(std::string(name));
   return pat;
 }
 
-void kore_composite_pattern_dump(kore_composite_pattern const *pat) {
-  pat->ptr_->print(std::cout);
+void kore_composite_pattern_add_argument(kore_pattern *pat, kore_pattern *arg) {
+  if (auto cast_ptr
+      = dynamic_cast<kllvm::KORECompositePattern *>(pat->ptr_.get())) {
+    cast_ptr->addArgument(
+        std::shared_ptr<kllvm::KOREPattern>(arg->ptr_.release()));
+  } else {
+    abort();
+  }
 }
 
-void kore_composite_pattern_add_argument(
-    kore_composite_pattern *pat, kore_composite_pattern *arg) {
-  pat->ptr_->addArgument(
-      std::shared_ptr<kllvm::KOREPattern>(arg->ptr_.release()));
-}
+/* KOREStringPattern */
 
-void kore_composite_pattern_free(kore_composite_pattern const *pat) {
-  delete pat;
+kore_pattern *kore_string_pattern_new(char const *contents) {
+  auto pat = new kore_pattern;
+  pat->ptr_ = kllvm::KOREStringPattern::Create(std::string(contents));
+  return pat;
 }
 }
