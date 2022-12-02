f572043be8210605a9223150b942fe340eeab3cd
Sam Balco <goodlyrottenapple@gmail.com>
2022-12-02 15:25:07 +0000

    added ifdef __cplusplus to the header file

--- bindings/c/include/kllvm-c/kllvm-c.h
+++ bindings/c/include/kllvm-c/kllvm-c.h
@@ -1,11 +1,15 @@
 #ifndef KLLVM_C_H
 #define KLLVM_C_H
 
+#ifdef __cplusplus
 extern "C" {
+#endif
+
 
 typedef struct kore_pattern kore_pattern;
 
 char *kore_pattern_dump(kore_pattern const *);
+
 void kore_pattern_free(kore_pattern const *);
 
 kore_pattern *kore_composite_pattern_new(char const *);
@@ -13,6 +17,8 @@ void kore_composite_pattern_add_argument(kore_pattern *, kore_pattern *);
 
 kore_pattern *kore_string_pattern_new(char const *);
 
+#ifdef __cplusplus
 }
+#endif
 
 #endif
