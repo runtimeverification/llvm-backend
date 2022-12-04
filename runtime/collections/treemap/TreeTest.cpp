62ad08f361f2a1b147ed5434d8a91c2aafa822ce
Maria Kotsifakou <kotsifakou.maria@gmail.com>
2022-12-04 14:21:40 -0600

    Temporaty tests.

--- runtime/collections/treemap/TreeTest.cpp
+++ runtime/collections/treemap/TreeTest.cpp
@@ -88,11 +88,130 @@ void makeTestVectors(int n, std::vector<int> &asc, std::vector<int> &desc, std::
     }
 }
 
+void testConcat()
+{
+    std::vector<RBTree<int, int> > op1;
+    op1.push_back(RBTree<int, int>());
+    std::vector<std::pair<int, int> > v1 =
+        {
+            std::pair<int, int>( 1, 1 ),
+            std::pair<int, int>( 3, 3 ),
+            std::pair<int, int>( 5, 5 ),
+            std::pair<int, int>( 7, 7 ),
+        };
+    op1.push_back(RBTree<int, int>(v1.begin(), v1.end()));
+for (RBTree r : op1)
+{
+print(r);
+}
+    std::vector<RBTree<int, int> > op2;
+    op2.push_back(RBTree<int, int>());
+    std::vector<std::pair<int, int> > v2 =
+        {
+            std::pair<int, int>( 2, 3 ),
+            std::pair<int, int>( 4, 5 ),
+            std::pair<int, int>( 6, 7 ),
+            std::pair<int, int>( 8, 9 ),
+        };
+    op2.push_back(RBTree<int, int>(v2.begin(), v2.end()));
+for (RBTree r : op2)
+{
+print(r);
+}
+
+    std::vector<RBTree<int, int> > res;
+    for (RBTree t1: op1)
+    {
+        for (RBTree t2: op2)
+        {
+            res.push_back(mapConcat(t1, t2));
+            res.push_back(mapConcat(t2, t1));
+        }
+    }
+    for (int i = 0; i < res.size(); i++)
+    {
+        RBTree<int, int> r = res[i];
+        r.assert1();
+        r.countB();
+        r.assertBST();
+std::cout << "tree " << i << " :" << std::endl;
+print(r);
+    }
+
+}
+
+void testInserted()
+{
+    std::vector<RBTree<int, int> > trees;
+    std::vector< std::vector<std::pair<int, int > > > ins;
+
+    trees.push_back(RBTree<int, int>());
+    std::vector<std::pair<int, int> > v1 =
+        {
+            std::pair<int, int>( 1, 1 ),
+            std::pair<int, int>( 3, 3 ),
+            std::pair<int, int>( 5, 5 ),
+            std::pair<int, int>( 7, 7 ),
+        };
+    trees.push_back(RBTree<int, int>(v1.begin(), v1.end()));
+    ins.push_back(v1);
+    v1.clear();
+    v1 =
+        {
+            std::pair<int, int>( 2, 2 ),
+            std::pair<int, int>( 4, 4 ),
+            std::pair<int, int>( 6, 6 ),
+            std::pair<int, int>( 8, 8 ),
+        };
+    ins.push_back(v1);
+    v1.clear();
+    v1 =
+        {
+            std::pair<int, int>( 1, 2 ),
+            std::pair<int, int>( 3, 4 ),
+            std::pair<int, int>( 5, 6 ),
+            std::pair<int, int>( 7, 8 ),
+        };
+    ins.push_back(v1);
+    v1.clear();
+    v1 =
+        {
+            std::pair<int, int>( 9, 1 ),
+            std::pair<int, int>( 9, 2 ),
+            std::pair<int, int>( 9, 3 ),
+        };
+    ins.push_back(v1);
+    v1.clear();
+
+for (RBTree r : trees)
+{
+print(r);
+}
+    std::vector<RBTree<int, int> > res;
+    for (RBTree t: trees)
+    {
+        for (std::vector<std::pair<int, int> > v: ins)
+        {
+            res.push_back(inserted(t, v.begin(), v.end()));
+        }
+    }
+    for (int i = 0; i < res.size(); i++)
+    {
+        RBTree<int, int> r = res[i];
+        r.assert1();
+        r.countB();
+        r.assertBST();
+std::cout << "tree " << i << " :" << std::endl;
+print(r);
+    }
+
+}
+
 int main()
 {
     testInit();
     std::vector<int> a, d, r;
-    makeTestVectors(10000, a, d, r);
+    makeTestVectors(20, a, d, r);
     std::cout << "--- Begin Asc test ---" << std::endl;
     hugeTest(a);
     std::cout << "--- End Asc test ---" << std::endl;
@@ -102,6 +221,12 @@ int main()
     std::cout << "--- Begin Rand test ---" << std::endl;
     hugeTest(r);
     std::cout << "--- End Rand test ---" << std::endl;
+    std::cout << "--- Begin Concat test ---" << std::endl;
+    testConcat();
+    std::cout << "--- End Concat test ---" << std::endl;
+    std::cout << "--- Begin Inserted test ---" << std::endl;
+    testInserted();
+    std::cout << "--- End Inserted test ---" << std::endl;
 
     return 0;
 
