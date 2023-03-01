def4dbe27510310b1c60ee48763cf222e9467839
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2023-03-01 17:42:46 -0300

    Using llvm::cl to get command line options from kprint

--- tools/kprint/main.cpp
+++ tools/kprint/main.cpp
@@ -1,33 +1,23 @@
 #include "kllvm/ast/AST.h"
 #include "kllvm/printer/printer.h"
+#include "llvm/Support/CommandLine.h"
 
 #include <iostream>
 #include <string>
 #include <unistd.h>
 
 using namespace kllvm;
+using namespace llvm;
 
-int main(int argc, char **argv) {
-  if (argc != 3 && argc != 4 && argc != 5) {
-    std::cerr
-        << "usage: " << argv[0]
-        << " <definition.kore> <pattern.kore> [true|false|auto] [true|false]"
-        << std::endl;
-  }
+cl::opt<std::string> DefinitionFilename(
+    cl::Positional, cl::desc("<definition.kore>"), cl::Required);
 
-  bool hasColor, filterSubst;
-  if (argc >= 4) {
-    std::string arg = argv[3];
-    hasColor = arg == "true" || (arg == "auto" && isatty(1));
-  } else {
-    hasColor = isatty(1);
-  }
-  if (argc >= 5) {
-    std::string arg = argv[4];
-    filterSubst = arg == "true";
-  } else {
-    filterSubst = true;
-  }
+cl::opt<std::string>
+    PatternFilename(cl::Positional, cl::desc("<pattern.kore>"), cl::Required);
 
-  printKORE(std::cout, argv[1], argv[2], hasColor, filterSubst);
+cl::opt<bool> FilterSubst("subs", cl::desc("Filter Substitutions"), cl::init("-"));
+
+int main(int argc, char **argv) {  
+  cl::ParseCommandLineOptions(argc, argv);
+  printKORE(std::cout, argv[1], argv[2], isatty(1), FilterSubst);
 }
