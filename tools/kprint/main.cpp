b0832c35602577c71f7e84677c090f128fdfc549
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2023-03-03 13:01:19 -0300

    Fixing small details on kprint implementation

--- tools/kprint/main.cpp
+++ tools/kprint/main.cpp
@@ -1,7 +1,6 @@
 #include "kllvm/ast/AST.h"
 #include "kllvm/printer/printer.h"
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/WithColor.h"
 
 #include <iostream>
 #include <string>
@@ -16,21 +15,20 @@ cl::opt<std::string> DefinitionFilename(
 cl::opt<std::string>
     PatternFilename(cl::Positional, cl::desc("<pattern.kore>"), cl::Required);
 
-cl::opt<std::string> FilterSubst(cl::Positional, cl::desc("[true|false]"));
+cl::opt<std::string>
+    ArgColor(cl::Positional, cl::desc("[true|false|auto]"), cl::init("auto"));
 
-cl::opt<std::string> ArgColor(cl::Positional, cl::desc("[true|false|auto]"));
+cl::opt<bool>
+    FilterSubst(cl::Positional, cl::desc("[true|false]"), cl::init(true));
 
 int main(int argc, char **argv) {
-  cl::PrintOptionValues();
-  StringMap<cl::Option *> &Map = cl::getRegisteredOptions();
-  Map["color"]->setHiddenFlag(cl::Hidden);
+  auto map = cl::getRegisteredOptions();
+  map["color"]->setHiddenFlag(cl::Hidden);
 
   cl::ParseCommandLineOptions(argc, argv);
 
-  bool hasColor = ArgColor == "true" || (ArgColor == "auto" && isatty(1))
-                  || (ArgColor == "" && isatty(1));
-  bool filterSubst = FilterSubst == "true" || "" == FilterSubst;
+  bool has_color = ArgColor == "true" || (ArgColor == "auto" && isatty(1));
 
   printKORE(
-      std::cout, DefinitionFilename, PatternFilename, hasColor, filterSubst);
+      std::cout, DefinitionFilename, PatternFilename, has_color, FilterSubst);
 }
