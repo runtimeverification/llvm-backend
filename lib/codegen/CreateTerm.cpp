cfc3eed82e16fb7ea8914264b707bb62761c045e
Bruce Collie <brucecollie82@gmail.com>
2023-07-24 11:04:58 +0100

    Clang 16, pointer check

--- lib/codegen/CreateTerm.cpp
+++ lib/codegen/CreateTerm.cpp
@@ -1154,8 +1154,7 @@ bool makeFunction(
   }
   CreateTerm creator = CreateTerm(subst, definition, block, Module, false);
   llvm::Value *retval = creator(pattern).first;
-  if (funcType->getReturnType()
-      == llvm::PointerType::getUnqual(retval->getType())) {
+  if (isCollectionSort(returnCat)) {
     auto tempAlloc = allocateTerm(
         retval->getType(), creator.getCurrentBlock(), "koreAllocAlwaysGC");
     new llvm::StoreInst(retval, tempAlloc, creator.getCurrentBlock());
