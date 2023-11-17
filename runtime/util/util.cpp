998285973c300dc8da72e86e2f0a545a4b3d4bb1
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2023-11-17 11:52:07 -0600

    Bump hint version for hook locations

--- runtime/util/util.cpp
+++ runtime/util/util.cpp
@@ -30,7 +30,7 @@ block *constructKItemInj(void *subject, const char *sort, bool raw_value) {
 }
 
 void printProofHintHeader(char *output_file) {
-  unsigned int version = 1;
+  unsigned int version = 2;
   FILE *file = fopen(output_file, "a");
   fprintf(file, "HINT");
   fwrite(&version, sizeof(version), 1, file);
