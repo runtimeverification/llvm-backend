5ca593b04cd15a3173cf09835eb2656f1264db64
Theodoros Kasampalis <kasampalis.theodoros@gmail.com>
2023-11-20 17:43:31 -0600

    C++ tool for proof hint format validation (#894)
    This PR adds a tool `kore-proof-trace` that validates the binary proof
    hint file outputed by an appropriately instrumented, llvm-backend
    generated interpreter. The tool accepts the `--verbose` option for
    printing some information about the trace. The tool returns `0` on a
    valid trace and `1` otherwise.
    
    ---------
    
    Co-authored-by: Bruce Collie <brucecollie82@gmail.com>

---
+++ tools/kore-proof-trace/main.cpp
@@ -0,0 +1,31 @@
+#include <kllvm/binary/ProofTraceValidator.h>
+
+#include <llvm/Support/CommandLine.h>
+
+#include <string>
+
+using namespace llvm;
+using namespace kllvm;
+
+cl::OptionCategory KoreProofTraceCat("kore-proof-trace options");
+
+cl::opt<std::string> InputFilename(
+    cl::Positional, cl::desc("<input file>"), cl::Required,
+    cl::cat(KoreProofTraceCat));
+
+cl::opt<bool> VerboseOutput(
+    "verbose",
+    llvm::cl::desc("Print verbose information about the input proof trace"),
+    llvm::cl::cat(KoreProofTraceCat));
+
+int main(int argc, char **argv) {
+  cl::HideUnrelatedOptions({&KoreProofTraceCat});
+  cl::ParseCommandLineOptions(argc, argv);
+
+  ProofTraceValidator Validator(VerboseOutput);
+  if (Validator.validate_proof_trace(InputFilename)) {
+    return 0;
+  }
+
+  return 1;
+}
