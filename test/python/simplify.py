b91e14839e4b1bfc9900679608b0a5e730aaf052
Bruce Collie <brucecollie82@gmail.com>
2023-11-30 12:09:14 +0000

    Reinstate missing binding

--- test/python/simplify.py
+++ test/python/simplify.py
@@ -10,17 +10,28 @@ import unittest
 
 class TestSimplify(unittest.TestCase):
 
-    def _check_simplification(self, label, value):
+    def _check_int_simplification(self, label, value):
         p = kllvm.parser.Parser.from_string(f'Lbl{label}{{}}()').pattern()
         s = kllvm.ast.CompositeSort('SortInt')
         result = kllvm.runtime.simplify_pattern(p, s)
         self.assertEqual(str(result), f'inj{{SortInt{{}}, SortKItem{{}}}}(\dv{{SortInt{{}}}}("{value}"))')
 
+    def _check_bool_simplification(self, label, value):
+        p = kllvm.parser.Parser.from_string(f'Lbl{label}{{}}()').pattern()
+        result = kllvm.runtime.simplify_bool_pattern(p)
+        self.assertTrue(result == value)
+
     def test_simplify_foo(self):
-        self._check_simplification('foo', 328)
+        self._check_int_simplification('foo', 328)
 
     def test_simplify_bar(self):
-        self._check_simplification('bar', 562)
+        self._check_int_simplification('bar', 562)
+
+    def test_simplify_baz(self):
+        self._check_bool_simplification('baz', True)
+
+    def test_simplify_qux(self):
+        self._check_bool_simplification('qux', False)
 
 if __name__ == "__main__":
     unittest.main()
