32f76e74a5517ad6e2f84b9f0d4055da3b877197
Bruce Collie <brucecollie82@gmail.com>
2024-02-07 09:55:26 +0000

    Experimental simplification of Nix / clang

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -17,15 +17,7 @@ let
       '';
     })
   else
-  # In llvmPackages_15/16, libcxx is broken, so we use clang 14 as our compiler
-  # for C code etc, but still use LLVM 15 to build the backend properly. This
-  # is a workaround until the underlying package is more stable on macOS.
-    let
-      clangPackages = if prev.llvm-version >= 15 then
-        mkLlvmPackages prev.llvmPackages_14
-      else
-        llvmPackages;
-    in clangPackages.libcxxClang.overrideAttrs (old: {
+    llvmPackages.libcxxClang.overrideAttrs (old: {
       # Hack from https://github.com/NixOS/nixpkgs/issues/166205 for macOS
       postFixup = old.postFixup + ''
         echo "-lc++abi" >> $out/nix-support/libcxx-ldflags
