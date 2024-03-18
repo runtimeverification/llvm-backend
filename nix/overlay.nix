5db8cb9aed9ef6cc466d66c1d8cec99856137aac
Bruce Collie <brucecollie82@gmail.com>
2024-03-18 11:33:38 +0000

    Add manual sources for scala compiler bridge

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -21,7 +21,7 @@ let
 
   llvm-backend-matching = import ./llvm-backend-matching.nix {
     src = prev.llvm-backend-matching-src;
-    mvnHash = "sha256-5wHyZF/a4seBo3gOHXkhNJimMyUaXxlSSbMeBH7ET7k=";
+    mvnHash = "sha256-2X8G3T05Pk1apA0f04Mdu/8DAB89oB9XwTBQ3KVoc/A=";
     inherit (final) maven;
   };
 
