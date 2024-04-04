e2538334d7651faf2bad32cfe962917a47c08c1f
Dwight Guth <dwight.guth@gmail.com>
2024-04-04 12:01:08 -0500

    fix compile error: temporary, not fully fixed

--- runtime/util/finish_rewriting.cpp
+++ runtime/util/finish_rewriting.cpp
@@ -44,7 +44,7 @@ int32_t get_exit_code(block *);
     } else {
       print_configuration(output_file, subject);
     }
-  } else if (!proof_hint_instrumentation_slow && !error) {
+  } else if (!error) {
     write_uint64_to_file(output_file, 0x33);
     serialize_configuration_to_file(output_file, subject, true, false);
     write_uint64_to_file(output_file, 0xCC);
