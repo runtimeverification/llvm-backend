c7371de16471ab6df9d8cad63a2885af52e88e6f
Dwight Guth <dwight.guth@runtimeverification.com>
2024-04-15 15:03:02 -0500

    modify load_<type>_for_gc and store_<type>_for_gc for new block layout

--- runtime/collect/collect.cpp
+++ runtime/collect/collect.cpp
@@ -348,7 +348,9 @@ bool store_map_for_gc(void **roots, map *ptr) {
   auto *hdr = (blockheader *)mem;
   std::string name = get_raw_symbol_name(kllvm::sort_category::Map) + "{}";
   *hdr = get_block_header_for_symbol(get_tag_for_symbol_name(name.c_str()));
-  auto *child = (map *)(hdr + 1);
+  auto offset = (uint64_t *)(hdr + 1);
+  *offset = 16;
+  auto *child = (map *)(hdr + 2);
   *child = std::move(*ptr);
   *roots = mem;
   return true;
@@ -363,7 +365,9 @@ bool store_set_for_gc(void **roots, set *ptr) {
   auto *hdr = (blockheader *)mem;
   std::string name = get_raw_symbol_name(kllvm::sort_category::Set) + "{}";
   *hdr = get_block_header_for_symbol(get_tag_for_symbol_name(name.c_str()));
-  auto *child = (set *)(hdr + 1);
+  auto offset = (uint64_t *)(hdr + 1);
+  *offset = 16;
+  auto *child = (set *)(hdr + 2);
   *child = std::move(*ptr);
   *roots = mem;
   return true;
@@ -378,7 +382,9 @@ bool store_list_for_gc(void **roots, list *ptr) {
   auto *hdr = (blockheader *)mem;
   std::string name = get_raw_symbol_name(kllvm::sort_category::List) + "{}";
   *hdr = get_block_header_for_symbol(get_tag_for_symbol_name(name.c_str()));
-  auto *child = (list *)(hdr + 1);
+  auto offset = (uint64_t *)(hdr + 1);
+  *offset = 16;
+  auto *child = (list *)(hdr + 2);
   *child = std::move(*ptr);
   *roots = mem;
   return true;
@@ -393,7 +399,9 @@ bool store_rangemap_for_gc(void **roots, rangemap *ptr) {
   auto *hdr = (blockheader *)mem;
   std::string name = get_raw_symbol_name(kllvm::sort_category::RangeMap) + "{}";
   *hdr = get_block_header_for_symbol(get_tag_for_symbol_name(name.c_str()));
-  auto *child = (rangemap *)(hdr + 1);
+  auto offset = (uint64_t *)(hdr + 1);
+  *offset = 16;
+  auto *child = (rangemap *)(hdr + 2);
   *child = std::move(*ptr);
   *roots = mem;
   return true;
@@ -402,7 +410,7 @@ bool store_rangemap_for_gc(void **roots, rangemap *ptr) {
 map *load_map_for_gc(void **roots, bool is_block) {
   void *mem = *roots;
   if (is_block) {
-    return (map *)(((char *)mem) + sizeof(blockheader));
+    return (map *)(((char *)mem) + sizeof(blockheader) + sizeof(uint64_t));
   }
   return (map *)mem;
 }
@@ -410,7 +418,7 @@ map *load_map_for_gc(void **roots, bool is_block) {
 set *load_set_for_gc(void **roots, bool is_block) {
   void *mem = *roots;
   if (is_block) {
-    return (set *)(((char *)mem) + sizeof(blockheader));
+    return (set *)(((char *)mem) + sizeof(blockheader) + sizeof(uint64_t));
   }
   return (set *)mem;
 }
@@ -418,7 +426,7 @@ set *load_set_for_gc(void **roots, bool is_block) {
 list *load_list_for_gc(void **roots, bool is_block) {
   void *mem = *roots;
   if (is_block) {
-    return (list *)(((char *)mem) + sizeof(blockheader));
+    return (list *)(((char *)mem) + sizeof(blockheader) + sizeof(uint64_t));
   }
   return (list *)mem;
 }
@@ -426,7 +434,7 @@ list *load_list_for_gc(void **roots, bool is_block) {
 rangemap *load_rangemap_for_gc(void **roots, bool is_block) {
   void *mem = *roots;
   if (is_block) {
-    return (rangemap *)(((char *)mem) + sizeof(blockheader));
+    return (rangemap *)(((char *)mem) + sizeof(blockheader) + sizeof(uint64_t));
   }
   return (rangemap *)mem;
 }
