4a8d7e210480de0eed3731437461b41296c74eec
Dwight Guth <dwight.guth@runtimeverification.com>
2024-05-01 13:02:24 -0500

    create fwrite equivalent for writer struct

--- include/runtime/header.h
+++ include/runtime/header.h
@@ -442,4 +442,16 @@ void sfprintf(writer *file, char const *fmt, Args &&...args) {
   }
 }
 
+template <typename... Args>
+void sfwrite(void const *ptr, size_t size, size_t nmemb, writer *file) {
+  if (file->file) {
+    fwrite(ptr, size, nmemb, file->file);
+  } else {
+    std::string output;
+    output.resize(size * nmemb);
+    memcpy(output.data(), ptr, size * nmemb);
+    hook_BUFFER_concat_raw(file->buffer, str.data(), str.size());
+  }
+}
+
 #endif // RUNTIME_HEADER_H
