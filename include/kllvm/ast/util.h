e2cc1cb622aca0b3e656ef56792920ab03c813d8
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2024-05-17 13:10:06 -0300

    Splitting utils header file in definitions

--- include/kllvm/ast/util.h
+++ include/kllvm/ast/util.h
@@ -1,28 +1,22 @@
+#ifndef UTIL_H
+#define UTIL_H
+
 #include <kllvm/ast/AST.h>
 
 #include <string>
 
-using namespace kllvm;
+namespace kllvm {
+
+/*
+ * This file contains utility functions that are used in multiple tools and
+ * are not specific to any one tool. These functions are used to manipulate
+ * the AST and other data structures.
+*/
 
 [[nodiscard]] std::optional<int64_t>
-get_start_line_location(kore_axiom_declaration const &axiom) {
-  auto location_att = axiom.attributes().get(attribute_set::key::Location);
-  assert(location_att->get_arguments().size() == 1);
+get_start_line_location(kore_axiom_declaration const &axiom);
 
-  auto str_pattern = std::dynamic_pointer_cast<kore_string_pattern>(
-      location_att->get_arguments()[0]);
-  std::string location = str_pattern->get_contents();
+[[nodiscard]] std::string trim(std::string s);
+} // namespace kllvm
 
-  size_t l_paren = location.find_first_of('(');
-  size_t first_comma = location.find_first_of(',');
-  size_t length = first_comma - l_paren - 1;
-  return std::stoi(location.substr(l_paren + 1, length));
-}
-
-// trim the string from the start
-[[nodiscard]] std::string trim(std::string s) {
-  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {
-            return !std::isspace(c);
-          }));
-  return s;
-}
+#endif // UTIL_H
---
+++ lib/ast/util.cpp
@@ -0,0 +1,27 @@
+#include "kllvm/ast/AST.h"
+#include "kllvm/ast/util.h"
+
+using namespace kllvm;
+
+[[nodiscard]] std::optional<int64_t>
+kllvm::get_start_line_location(kore_axiom_declaration const &axiom) {
+  auto location_att = axiom.attributes().get(attribute_set::key::Location);
+  assert(location_att->get_arguments().size() == 1);
+
+  auto str_pattern = std::dynamic_pointer_cast<kore_string_pattern>(
+      location_att->get_arguments()[0]);
+  std::string location = str_pattern->get_contents();
+
+  size_t l_paren = location.find_first_of('(');
+  size_t first_comma = location.find_first_of(',');
+  size_t length = first_comma - l_paren - 1;
+  return std::stoi(location.substr(l_paren + 1, length));
+}
+
+// trim the string from the start
+[[nodiscard]] std::string kllvm::trim(std::string s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {
+            return !std::isspace(c);
+          }));
+  return s;
+}
