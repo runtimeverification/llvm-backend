61cd926a12be123b616dd6fa61e6a8b4e72288b6
Dwight Guth <dwight.guth@runtimeverification.com>
2024-07-08 13:43:21 -0500

    fix bug in ci-tests.sh

--- .github/workflows/ci-tests.sh
+++ .github/workflows/ci-tests.sh
@@ -10,6 +10,7 @@ pushd build
 cmake ..                                                  \
   -DCMAKE_C_COMPILER="$(which clang-${llvm_version})"     \
   -DCMAKE_CXX_COMPILER="$(which clang++-${llvm_version})" \
+  -DCMAKE_BUILD_TYPE=Debug                                \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=On                      \
   -DBUILD_TESTS=On                                        \
   -DCMAKE_INSTALL_PREFIX=install
