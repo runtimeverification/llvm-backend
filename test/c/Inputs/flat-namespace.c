be6aee2632f7328ed5b0af88f727c35fbcc5741a
Bruce Collie <brucecollie82@gmail.com>
2024-07-19 13:12:14 +0100

    Clarifying comment

--- test/c/Inputs/flat-namespace.c
+++ test/c/Inputs/flat-namespace.c
@@ -3,6 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * The K program corresponding to this test will evaluate the `Int2Bytes` hooked
+ * function when it is run; this hook ends up allocating and freeing a temporary
+ * MPZ integer locally. If we're in a situation where `-flat_namespace` has been
+ * enabled on macOS, it's possible for the hook to end up resolving
+ * `__gmpz_clear` to a symbol defined in the host binary (rather than libgmp's
+ * dynamic library!).
+ *
+ * This originally manifested when the HB booster loaded a C bindings library
+ * (the booster statically links libgmp and so contains a symbol with this
+ * name); this test is a minimised reproduction of that issue.
+ */
 void __gmpz_clear(void *p) {
   abort();
 }
