27a7da289911584fce028905d0d1e74dc84e7696
Roberto Rosmaninho <roberto.rosmaninho@pi2.network>
2024-09-06 15:03:06 -0300

    Formating and deleting usus of FWD_PTR_BIT

--- runtime/meta/kreflection.cpp
+++ runtime/meta/kreflection.cpp
@@ -78,22 +78,18 @@ block *copy_block_to_eternal_lifetime(block *term) {
     case STRINGBUFFER_LAYOUT: {
       auto new_buffer = (stringbuffer *)new_block;
       auto buffer = (stringbuffer *)arg;
-      uint64_t const hdr = buffer->h.hdr;
-      bool hasForwardingAddress = hdr & FWD_PTR_BIT;
 
-      if (!hasForwardingAddress) {
-        uint64_t const cap = len(buffer->contents);
-        string *new_contents
-            = (string *)kore_alloc_token_forever(sizeof(string) + cap);
+      uint64_t const cap = len(buffer->contents);
+      string *new_contents
+          = (string *)kore_alloc_token_forever(sizeof(string) + cap);
 
-        memcpy(new_contents, buffer->contents, sizeof(string) + buffer->strlen);
-        memcpy(new_buffer, buffer, sizeof(stringbuffer));
+      memcpy(new_contents, buffer->contents, sizeof(string) + buffer->strlen);
+      memcpy(new_buffer, buffer, sizeof(stringbuffer));
 
-        new_buffer->h.hdr |= NOT_YOUNG_OBJECT_BIT;
-        new_buffer->contents = new_contents;
-        *(stringbuffer **)(buffer->contents) = new_buffer;
-        buffer->h.hdr |= FWD_PTR_BIT;
-      }
+      new_buffer->h.hdr |= NOT_YOUNG_OBJECT_BIT;
+      new_buffer->contents = new_contents;
+      *(stringbuffer **)(buffer->contents) = new_buffer;
+
       *(stringbuffer **)(((char *)new_block) + arg_data->offset)
           = *(stringbuffer **)(buffer->contents);
       break;
@@ -101,35 +97,29 @@ block *copy_block_to_eternal_lifetime(block *term) {
     case INT_LAYOUT: {
       mpz_ptr *ptr = static_cast<mpz_ptr *>((mpz_ptr *)arg);
       mpz_hdr *new_arg = STRUCT_BASE(mpz_hdr, i, *ptr);
-      uint64_t const hdr = new_arg->h.hdr;
-      bool hasForwardingAddress = hdr & FWD_PTR_BIT;
 
-      if (!hasForwardingAddress) {
-        mpz_hdr *new_intgr = nullptr;
-        string *new_limbs = nullptr;
-        bool has_limbs = new_arg->i->_mp_alloc > 0;
-        if (has_limbs) {
-          string *limbs = STRUCT_BASE(string, data, new_arg->i->_mp_d);
-          size_t len_limbs = len(limbs);
+      mpz_hdr *new_intgr = nullptr;
+      string *new_limbs = nullptr;
+      bool has_limbs = new_arg->i->_mp_alloc > 0;
+      if (has_limbs) {
+        string *limbs = STRUCT_BASE(string, data, new_arg->i->_mp_d);
+        size_t len_limbs = new_arg->i->_mp_alloc * sizeof(mp_limb_t);
 
-          assert(new_arg->i->_mp_alloc * sizeof(mp_limb_t) == len_limbs);
+        new_intgr = STRUCT_BASE(mpz_hdr, i, kore_alloc_integer_forever(0));
+        new_limbs
+            = (string *)kore_alloc_token_forever(sizeof(string) + len_limbs);
 
-          new_intgr = STRUCT_BASE(mpz_hdr, i, kore_alloc_integer_forever(0));
-          new_limbs
-              = (string *)kore_alloc_token_forever(sizeof(string) + len_limbs);
-
-          memcpy(new_limbs, limbs, sizeof(string) + len_limbs);
-        } else {
-          new_intgr = STRUCT_BASE(mpz_hdr, i, kore_alloc_integer_forever(0));
-        }
-        memcpy(new_intgr, new_arg, sizeof(mpz_hdr));
-        new_intgr->h.hdr |= NOT_YOUNG_OBJECT_BIT;
-        if (has_limbs) {
-          new_intgr->i->_mp_d = (mp_limb_t *)new_limbs->data;
-        }
-        *(mpz_ptr *)(&new_arg->i->_mp_d) = new_intgr->i;
-        new_arg->h.hdr |= FWD_PTR_BIT;
+        memcpy(new_limbs, limbs, sizeof(string) + len_limbs);
+      } else {
+        new_intgr = STRUCT_BASE(mpz_hdr, i, kore_alloc_integer_forever(0));
       }
+      memcpy(new_intgr, new_arg, sizeof(mpz_hdr));
+      new_intgr->h.hdr |= NOT_YOUNG_OBJECT_BIT;
+      if (has_limbs) {
+        new_intgr->i->_mp_d = (mp_limb_t *)new_limbs->data;
+      }
+      *(mpz_ptr *)(&new_arg->i->_mp_d) = new_intgr->i;
+
       *ptr = *(mpz_ptr *)(&new_arg->i->_mp_d);
       *(void **)(((char *)new_block) + arg_data->offset) = *(void **)ptr;
       break;
@@ -137,36 +127,32 @@ block *copy_block_to_eternal_lifetime(block *term) {
     case FLOAT_LAYOUT: {
       floating **floating_ptr = static_cast<floating **>((floating **)arg);
       floating_hdr *flt = STRUCT_BASE(floating_hdr, f, *floating_ptr);
-      uint64_t const hdr = flt->h.hdr;
-      bool hasForwardingAddress = hdr & FWD_PTR_BIT;
 
-      if (!hasForwardingAddress) {
-        floating_hdr *new_flt = nullptr;
-        string *new_limbs = nullptr;
-        string *limbs = STRUCT_BASE(string, data, flt->f.f->_mpfr_d - 1);
-        size_t len_limbs = len(limbs);
+      floating_hdr *new_flt = nullptr;
+      string *new_limbs = nullptr;
+      string *limbs = STRUCT_BASE(string, data, flt->f.f->_mpfr_d - 1);
+      size_t len_limbs = len(limbs);
 
-        assert(
-            ((flt->f.f->_mpfr_prec + mp_bits_per_limb - 1) / mp_bits_per_limb)
-                * sizeof(mp_limb_t)
-            <= len_limbs);
+      assert(
+          ((flt->f.f->_mpfr_prec + mp_bits_per_limb - 1) / mp_bits_per_limb)
+              * sizeof(mp_limb_t)
+          <= len_limbs);
 
-        new_flt = STRUCT_BASE(floating_hdr, f, kore_alloc_floating_forever(0));
-        new_limbs
-            = (string *)kore_alloc_token_forever(sizeof(string) + len_limbs);
+      new_flt = STRUCT_BASE(floating_hdr, f, kore_alloc_floating_forever(0));
+      new_limbs
+          = (string *)kore_alloc_token_forever(sizeof(string) + len_limbs);
 
-        memcpy(new_limbs, limbs, sizeof(string) + len_limbs);
-        memcpy(new_flt, flt, sizeof(floating_hdr));
+      memcpy(new_limbs, limbs, sizeof(string) + len_limbs);
+      memcpy(new_flt, flt, sizeof(floating_hdr));
 
-        new_flt->h.hdr |= NOT_YOUNG_OBJECT_BIT;
-        new_flt->f.f->_mpfr_d = (mp_limb_t *)new_limbs->data + 1;
+      new_flt->h.hdr |= NOT_YOUNG_OBJECT_BIT;
+      new_flt->f.f->_mpfr_d = (mp_limb_t *)new_limbs->data + 1;
 
-        *(floating **)(flt->f.f->_mpfr_d) = &new_flt->f;
-        flt->h.hdr |= FWD_PTR_BIT;
-      }
+      *(floating **)(flt->f.f->_mpfr_d) = &new_flt->f;
 
       *floating_ptr = *(floating **)(flt->f.f->_mpfr_d);
-      *(void **)(((char *)new_block) + arg_data->offset) = *(void **)floating_ptr;
+      *(void **)(((char *)new_block) + arg_data->offset)
+          = *(void **)floating_ptr;
       break;
     }
     case BOOL_LAYOUT: {
