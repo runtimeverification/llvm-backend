de41beb0fe4b61b7b97ed00b2bda182d57c9bec1
Dwight Guth <dwight.guth@pi2.network>
2025-05-05 14:06:10 -0500

    revert something that works locally but crashes on ci, probably due to libc version

--- runtime/collect/migrate_static_roots.cpp
+++ runtime/collect/migrate_static_roots.cpp
@@ -2,7 +2,7 @@
 
 #include "runtime/collect.h"
 
-extern thread_local constinit std::vector<BlockEnumerator> block_enumerators;
+extern thread_local std::vector<BlockEnumerator> block_enumerators;
 
 extern thread_local gmp_randstate_t kllvm_rand_state;
 extern thread_local constinit bool kllvm_rand_state_initialized;
