e0c965c2aa64f679d178b5b9d9952e1a4f055417
Roberto Rosmaninho <roberto.rosmaninho@pi2.network>
2025-05-28 17:13:56 -0300

    Deleting munmap from collect.cpp

--- runtime/collect/collect.cpp
+++ runtime/collect/collect.cpp
@@ -346,8 +346,5 @@ void kore_collect(
 void free_all_kore_mem() {
   kore_collect(nullptr, 0, nullptr, true);
   kore_clear();
-  youngspace.munmap_arena_and_reset();
-  oldspace.munmap_arena_and_reset();
-  alwaysgcspace.munmap_arena_and_reset();
 }
 }
