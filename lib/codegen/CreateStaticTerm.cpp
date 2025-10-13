29718e4f619f82ce5dbebcad60e92641a4d5f377
Jost Berthold <jost.berthold@gmail.com>
2025-10-13 12:25:45 +1100

    prefix the escaped kore

--- lib/codegen/CreateStaticTerm.cpp
+++ lib/codegen/CreateStaticTerm.cpp
@@ -37,7 +37,7 @@ llvm::Constant *create_static_term::not_injection_case(
   std::stringstream kore_string;
   constructor->print(kore_string);
   llvm::Constant *block
-      = module_->getOrInsertGlobal(escape(kore_string.str()), block_type);
+      = module_->getOrInsertGlobal("const_" + escape(kore_string.str()), block_type);
   auto *global_var = llvm::dyn_cast<llvm::GlobalVariable>(block);
   // this is technically not a constant because functions which return fresh constants
   // will mutate a block in this circumstance. Probably best not to rely on this actually
